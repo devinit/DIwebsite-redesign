25100268417164bb3516ffaf2a9e0285201caa38
Ben Coleman <ben@bteam.co>
2019-06-07 20:58:24 +0100

    Sorted the maps

--- patterns/assets/js/utils/gmaps.js
+++ patterns/assets/js/utils/gmaps.js
@@ -14,12 +14,26 @@ export default function initGmaps (selector = '.media-map__location', APIKey = u
     const [lat, lng] = $el.attr('data-latlng').split(',').map(val => parseFloat(val));
     const latlng = {lat, lng};
 
-    console.log(latlng);
+    // Set up your styles here
+    const styles = [
+        {
+            "featureType": "administrative.country",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                },
+                {
+                    "hue": "#ff0000"
+                }
+            ]
+        }
+    ]
 
     gmaps.load(google => {
         const opts = {
             center: latlng,
-            zoom: 2,
+            zoom: 3,
             zoomControl: true,
             zoomControlOptions: {
                 style: google.maps.ZoomControlStyle.SMALL,
@@ -36,22 +50,9 @@ export default function initGmaps (selector = '.media-map__location', APIKey = u
                 opened: false,
             },
             mapTypeId: google.maps.MapTypeId.ROADMAP,
-            styles: [
-                {
-                    "featureType": "administrative.country",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "visibility": "simplified"
-                        },
-                        {
-                            "hue": "#ff0000"
-                        }
-                    ]
-                }
-            ],
+            styles: styles,
         };
-        const locations = $('.media-map__location')
+        const locations = $('.media-map__location__point')
             .map(function() {
                 const [lat, lng] = $(this).data('latlng').split(',').map(val => parseFloat(val));
                 const latlng = {lat, lng};
@@ -68,17 +69,41 @@ export default function initGmaps (selector = '.media-map__location', APIKey = u
             .get();
 
         const map = new google.maps.Map(el, opts);
+        const infoWindow = new google.maps.InfoWindow();
 
-        locations.forEach(location => new google.maps.Marker({
-            icon: location.icon,
-            position: location.position,
-            map: map,
-            title: location.title,
-            desc: location.desc,
-            tel: location.tel,
-            email: location.email,
-            web: location.web,
-        }));
+        // Based on https://www.aspsnippets.com/Articles/Google-Maps-API-V3-Center-and-Zoom-to-fit-all-markers-on-Google-Maps.aspx
+
+        var latlngbounds = new google.maps.LatLngBounds();
+
+        for (var i = 0; i < locations.length; i++) {
+            var data = locations[i];
+            var myLatlng = new google.maps.LatLng(data.position);
+            var location = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: data.title,
+                icon: data.icon,
+                desc: data.desc,
+                tel: data.tel,
+                email: data.email,
+                web: data.web,
+                animation: google.maps.Animation.DROP
+            });
+            (function (location, data) {
+                google.maps.event.addListener(location, "click", function (e) {
+                    infoWindow.setContent('<div style="min-width:60px;">' + data.title + '</div>');
+                    infoWindow.open(map, location);
+                });
+            })(location, data);
+            latlngbounds.extend(location.position);
+        }
+
+        //Get the boundaries of the Map.
+        var bounds = new google.maps.LatLngBounds();
+
+        //Center map and adjust Zoom based on the position of all markers.
+        map.setCenter(latlngbounds.getCenter());
+        map.fitBounds(latlngbounds);
 
     });
 

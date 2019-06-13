d4a7642f7273c27f4b9181a373626785a8f200e7
Alex Miller <alex.k.miller@gmail.com>
2019-06-13 13:50:27 -0400

    dj_database_url setup

--- di_website/settings/base.py
+++ di_website/settings/base.py
@@ -90,10 +90,7 @@ WSGI_APPLICATION = 'di_website.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 

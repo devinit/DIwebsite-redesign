70cf2839fc8d3c0b4ad7c9ddab883158580e55bd
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-06-18 15:36:28 +0300

    [New] dynamic navigation header #42

--- di_website/context.py
+++ di_website/context.py
@@ -1,5 +1,6 @@
 import os
 from django.conf import settings
+from navigation.models import PrimaryMenu
 
 def globals(request):
 
@@ -11,5 +12,11 @@ def globals(request):
             'DEBUG': bool(os.getenv('DEBUG', False)),
             'site_name': request.site.site_name or settings.WAGTAIL_SITE_NAME,
             'assets_root': '/assets',
+            'primary_menu': construct_nav(PrimaryMenu.for_site(request.site).primary_menu_links.all()),
         },
     }
+
+
+def construct_nav(qs):
+    nav = list(qs)
+    return nav

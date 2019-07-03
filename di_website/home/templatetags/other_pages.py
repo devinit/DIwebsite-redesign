6642839268533f409691bb05a68ed4f331d2e1e2
Alex Miller <alex.k.miller@gmail.com>
2019-07-03 13:35:34 -0400

    Solve other page TODO'

--- di_website/home/templatetags/other_pages.py
+++ di_website/home/templatetags/other_pages.py
@@ -1,19 +1,25 @@
 from django import template
 
 from wagtail.core.models import Page
+from di_website.home.models import HomePage
+
 
 register = template.Library()
 
-"""
-Get all the other pages that make up part of the menu
-"""
-@register.inclusion_tag('includes/cards/other_pages.html', takes_context=True)
-def get_other_pages(context):
-    
-    # TODO Filter out the current page, root page and home page, 
-    # TODO Also filter out child pages of the children of home page
 
-    published_pages = Page.objects.live()
+@register.inclusion_tag('includes/cards/other_pages.html', takes_context=True)
+def get_other_pages(context, calling_page=None):
+    """
+    Get all the other pages that make up part of the menu
+    """
+    root_page = context.request.site.root_page.get_root()
+    home_excludes = root_page.get_descendants(inclusive=True).filter(depth__lte=4) #  Root=1, Home=2, Homechild=3, Homegrandchild=4
+    exclude_pks = [desc_page.pk for desc_page in home_excludes]
+
+    if calling_page:
+        exclude_pks.append(calling_page.pk)
+
+    published_pages = Page.objects.live().exclude(pk__in=exclude_pks).specific()
     return {
-        'published_pages':published_pages
+        'published_pages': published_pages
     }

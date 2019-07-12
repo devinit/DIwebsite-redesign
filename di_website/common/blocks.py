33425146ea20930de474467af0b3e36ffba1d4ba
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-07-12 13:47:55 +0300

    [Update] paragraph_block

--- di_website/common/blocks.py
+++ di_website/common/blocks.py
@@ -1,6 +1,6 @@
 from wagtail.core.blocks import (
-    StructBlock,
-    TextBlock,
+    BooleanBlock,
+    CharBlock,
     RichTextBlock,
     StreamBlock,
     StructBlock,
@@ -17,8 +17,25 @@ class BlockQuote(StructBlock):
     text = TextBlock()
 
     class Meta:
-        icon = "fa-quote-left"
-        template = "blocks/blockquote.html"
+        icon = 'fa-quote-left'
+        template = 'blocks/blockquote.html'
+
+
+class ParagraphBlock(StructBlock):
+    text = RichTextBlock(
+        icon='fa-paragraph',
+        features=RICHTEXT_FEATURES
+    )
+    is_typeset = BooleanBlock(required=False)
+    is_section = BooleanBlock(required=False)
+    classname = CharBlock(
+        required=False,
+        help_text='e.g. vacancy pages have a "is-typeset--additionals" class that customises particular areas'
+    )
+
+    class Meta():
+        template = 'blocks/paragraph_block.html'
+        icon="fa-paragraph"
 
 
 # StreamBlocks
@@ -26,9 +43,5 @@ class BaseStreamBlock(StreamBlock):
     """
     Define the custom blocks that `StreamField` will utilize
     """
-    paragraph_block = RichTextBlock(
-        icon="fa-paragraph",
-        template="blocks/paragraph_block.html",
-        features=RICHTEXT_FEATURES
-    )
+    paragraph_block = ParagraphBlock()
     block_quote = BlockQuote()

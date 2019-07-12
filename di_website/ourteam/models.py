51485d2b53c79bea6e03da504eb15f1dbcb1ccf2
Naphlin Peter Akena <starnapho@gmail.com>
2019-07-12 21:03:21 +0300

    Changed implementation for UserProfile

--- di_website/ourteam/models.py
+++ di_website/ourteam/models.py
@@ -1,34 +1,109 @@
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel
+from django.shortcuts import render
+from django.utils.text import slugify
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
 from wagtail.core.models import Page
 from di_website.common.base import StandardPage
+from di_website.users.models import Department,JobTitle
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+from django.contrib.auth.models import User
+from wagtail.images.edit_handlers import ImageChooserPanel
 
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 
-class OurTeamPage(StandardPage):
+class OurTeamIndexPage(RoutablePageMixin,StandardPage):
 
     """ List of Team Members Page """
+    template="ourteam/our_team_page.html"
+
+    def get_context(self, request):
+        context = super(OurTeamIndexPage, self).get_context(request)
+        context['profiles'] = TeamMemberPage.objects.live()
+        context['departments'] = Department.objects.all()
+
+        return context
+
+    @route(r"(team-filter=\w+)$", name='team_filter')
+    def team_filter(self, request, filter_query=None):
+        department = Department.objects.get(slug=filter_query)
+        profiles = TeamMemberPage.object.get(department_id = department.id)
+        context = super(OurTeamIndexPage, self).get_context(request)
+        context['profiles'] = profiles
+        context['departments'] = Department.objects.all()
+
+        return render(request, self.template, context)
 
     class Meta:
         verbose_name = "Our Team Page"
 
+    subpage_types = ['TeamMemberPage']
+
+   
+
 
 class TeamMemberPage(Page):
     """Individual team member page. Autocreated when profile is."""
-    user_profile = models.ForeignKey(
-        'users.UserProfile',
+    user = models.OneToOneField(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    name = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    position = models.ForeignKey(
+        JobTitle,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    department = models.ForeignKey(
+        Department,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    email = models.EmailField(
+        null=True,
+        blank=True
+    )
+    telephone = models.CharField(max_length=255, null=True, blank=True)
+    active = models.BooleanField(default=False, help_text="Should this user's profile be displayed as staff?")
 
+    content_panels = [
+        FieldPanel('user'),
+        FieldPanel('name'),
+        ImageChooserPanel('image'),
+        SnippetChooserPanel('position'),
+        SnippetChooserPanel('department'),
+        FieldPanel('email'),
+        FieldPanel('telephone'),
+        FieldPanel('active')
+    ]
+
+    def clean(self):
+        super().clean()
+        self.title = self.name
+        self.slug = slugify(self.name) 
+
+    def __str__(self):
+        return "{} ({})".format(self.name if self.name else self.user.username, "Active" if self.active else "Inactive")
+
+    
     class Meta:
         verbose_name = "Team Member Page"
-
-    content_panels = Page.content_panels + [
-        FieldPanel('user_profile')
-    ]
+    
 
     parent_page_types = [
-        'OurTeamPage'
+        'OurTeamIndexPage'
     ]
+
+TeamMemberPage._meta.get_field('slug').default = 'default-blank-slug'
--- di_website/users/models.py
+++ di_website/users/models.py
@@ -1,21 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from wagtail.snippets.models import register_snippet
 from wagtail.admin.edit_handlers import (
     FieldPanel,
-    PageChooserPanel
 )
-from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
-from di_website.ourteam.models import OurTeamPage, TeamMemberPage
-from di_website.common.edit_handlers import ReadOnlyPanel
-
-import logging
-logger = logging.getLogger('django')
 
 @register_snippet
 class JobTitle(models.Model):
@@ -46,112 +37,3 @@ class Department(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         super(Department, self).save(*args, **kwargs)
-
-
-@register_snippet
-class UserProfile(models.Model):
-    # TODO: add paragraph when added to common
-    user = models.OneToOneField(
-        User,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
-    name = models.CharField(max_length=255, null=True, blank=True)
-    image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    position = models.ForeignKey(
-        JobTitle,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    department = models.ForeignKey(
-        Department,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    page = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    email = models.EmailField(
-        null=True,
-        blank=True
-    )
-    telephone = models.CharField(max_length=255, null=True, blank=True)
-    active = models.BooleanField(default=False, help_text="Should this user's profile be displayed as staff?")
-
-    panels = [
-        FieldPanel('user'),
-        ReadOnlyPanel('name',heading='name',help_text='To edit, change firstname and lastname in user settings'),
-        ImageChooserPanel('image'),
-        SnippetChooserPanel('position'),
-        SnippetChooserPanel('department'),
-        PageChooserPanel('page'),
-        FieldPanel('email'),
-        FieldPanel('telephone'),
-        FieldPanel('active')
-    ]
-
-    def __str__(self):
-        return "{} ({})".format(self.name if self.name else self.user.username, "Active" if self.active else "Inactive")
-
-    class Meta:
-        verbose_name = 'User Profile'
-        verbose_name_plural = 'User Profiles'
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    """Create a new user profile on a post-save."""
-    profile, _ = UserProfile.objects.get_or_create(user=instance)
-    
-    profile.name = instance.get_full_name()
-    if not profile.email:
-        profile.email = instance.email
-    profile.save()
-
-    team_page = OurTeamPage.objects.all().live().first()
-    if team_page:
-        if not profile.page:
-            team_member_page = TeamMemberPage(title=profile.name, slug=slugify(profile.name), user_profile=profile)
-            team_page.add_child(instance=team_member_page)
-            team_member_page.save_revision().publish()
-            profile.page = team_member_page
-            profile.save()
-        else:
-            team_member_page = TeamMemberPage.objects.get(user_profile_id=profile.id)
-            if team_member_page:
-                team_member_page.slug=slugify(profile.name)
-                team_member_page.save()
-        
-
-@receiver(post_save, sender=UserProfile)
-def manage_memeber_page(sender, instance, created, **kwargs):
-    try:
-        if instance.page and not instance.active:
-            teammemberpage = TeamMemberPage.objects.get(slug=slugify(instance.name))
-            teammemberpage.live = instance.active
-            teammemberpage.save()
-
-        if instance.page and instance.active:
-            teammemberpage = TeamMemberPage.objects.get(slug=slugify(instance.name))
-            teammemberpage.live = instance.active 
-            teammemberpage.save()
-    except:
-        # Exception expected from double save
-        pass
-   
-

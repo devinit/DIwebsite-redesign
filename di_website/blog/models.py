0200741ff3d4e7da29c3c0759b50c35619c8b22a
Alex Miller <alex.k.miller@gmail.com>
2019-07-15 13:35:23 -0400

    get_context for blogs

--- di_website/blog/models.py
+++ di_website/blog/models.py
@@ -1,19 +1,39 @@
 from django.db import models
-from di_website.common.base import StandardPage
+from django.utils.text import slugify
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+from di_website.common.base import StandardPage
 
 
 @register_snippet
 class BlogTopic(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, blank=True, null=True, help_text="Optional. Will be auto-generated from name if left blank.")
 
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(BlogTopic, self).save(*args, **kwargs)
+
 
 class BlogIndexPage(StandardPage):
     subpage_types = ['BlogArticlePage']
+
+    def get_context(self, request):
+        context = super(BlogIndexPage, self).get_context(request)
+        topic_filter = request.GET.get('topic', None)
+        if topic_filter:
+            context['articles'] = BlogArticlePage.objects.live().filter(topic__slug=topic_filter)
+        else:
+            context['articles'] = BlogArticlePage.objects.live()
+        context['topics'] = BlogTopic.objects.all()
+        context['selected_topic'] = topic_filter
+
+        return context
 
 
 class BlogArticlePage(StandardPage):
--- di_website/blog/templatetags/__init__.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from di_website.blog.models import BlogArticlePage
-
-register = template.Library()
-
-
-@register.simple_tag
-def get_blog_articles():
-    return BlogArticlePage.objects.all().live()

55ca3471c0c9a77913446d7542967fc1393029fd
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-07-16 16:32:25 +0300

    [New] email settings

--- di_website/settings/base.py
+++ di_website/settings/base.py
@@ -170,3 +170,14 @@ WAGTAIL_SITE_NAME = "di_website"
 BASE_URL = 'http://devinit.org'
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = DEFAULT_FROM_EMAIL

7e6f18c06242134f133f849543433989d6a38408
Alex Miller <alex.k.miller@gmail.com>
2019-07-16 14:40:17 -0400

    Don't erase project dir

--- deploy.sh
+++ deploy.sh
@@ -13,27 +13,12 @@ docker-compose down
 
 # Move back to root director
 printf  "Cloning new content from active branch "$deploy_branch"\n\n"
-cd ~
-rm -rf $app_dir
-git clone git@github.com:devinit/DIwebsite-redesign.git -b $deploy_branch
+git fetch
+git stash
+git checkout $deploy_branch
+git reset --hard origin/$deploy_branch
 
 printf "Starting up services.... \n\n"
-cd $app_dir
-
-printf  "Checking if docker database volume exists .... \n\n"
-
-volume_status=$(docker volume ls)
-
-printf $volume_status
-
-if [ $(grep $docker_volume_name $volume_status) -ne 0 ];then
-    printf  "Volume already exists, skipping creation of new volume\n\n"
-else
-   printf  "No docker volume found, creating new volume for deployment\n\n"
-   docker volume create --name=$docker_volume_name
-fi
-
 echo "DATABASE_URL=postgresql://di_website:di_website_pw@db/di_website" > .env
-
 docker-compose up -d --build
 

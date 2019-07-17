6c2f44efca045b0ae67364cf2610d9e5dc9a27b0
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-07-17 16:29:14 +0300

    [Update] add file processing functions to string_utils

--- di_website/common/templatetags/string_utils.py
+++ di_website/common/templatetags/string_utils.py
@@ -1,4 +1,5 @@
 import uuid
+import mimetypes
 
 from django import template
 
@@ -7,3 +8,31 @@ register = template.Library()
 @register.simple_tag
 def uid():
     return str(uuid.uuid4())[:6]
+
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Y', suffix)
+
+
+@register.filter
+def file_info(file):
+    try:
+        return '%s %s' % (mimetypes.guess_extension(mimetypes.guess_type(file.url)[0])[1:].upper(), sizeof_fmt(file.file.size))
+    except Exception:
+        return file.title
+
+
+@register.filter
+def file_label(file):
+    try:
+        return '%s | %s %s' % (
+            file.title,
+            mimetypes.guess_extension(mimetypes.guess_type(file.url)[0])[1:].upper(),
+            sizeof_fmt(file.file.size)
+        )
+    except Exception:
+        return file.title

be853f5ceef8f8043d6f0d71c5156969716da825
Alex Miller <alex.k.miller@gmail.com>
2019-07-23 04:41:32 -0400

    Blog tests to avoid regression

--- di_website/tests.py
+++ di_website/tests.py
@@ -1,7 +1,10 @@
-from django.test import override_settings
-from wagtail.tests.utils import WagtailPageTests
-from di_website.home.models import HomePage
 from django.contrib.auth.models import User
+from django.test import override_settings
+
+from wagtail.tests.utils import WagtailPageTests
+
+from di_website.blog.models import BlogIndexPage, BlogArticlePage
+from di_website.home.models import HomePage
 from di_website.ourteam.models import OurTeamPage, TeamMemberPage
 from di_website.users.models import Department
 
@@ -58,3 +61,30 @@ class TestUserProfileCreation(WagtailPageTests):
     def test_department_filter_negative(self):
         response = self.client.get(self.team_page.url+"?team-filter="+self.dummy_department.slug)
         self.assertTrue(self.team_member_page.name not in str(response.content))
+
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class TestBlogPages(WagtailPageTests):
+    def setUp(self):
+        self.blog_index_page = BlogIndexPage(title="Test Blog Index Page", slug="blog")
+        home_page = HomePage.objects.first()
+        home_page.add_child(instance=self.blog_index_page)
+
+        self.blog_article_page = BlogArticlePage(title="Test Blog Article", slug="test")
+        self.blog_index_page.add_child(instance=self.blog_article_page)
+
+    def test_blog_index_page_200(self):
+        response = self.client.get(self.blog_index_page.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_index_page_renders(self):
+        response = self.client.get(self.blog_index_page.url)
+        self.assertTrue(self.blog_index_page.title in str(response.content))
+
+    def test_blog_article_page_200(self):
+        response = self.client.get(self.blog_article_page.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_article_page_renders(self):
+        response = self.client.get(self.blog_article_page.url)
+        self.assertTrue(self.blog_article_page.title in str(response.content))

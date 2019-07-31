4b3fd55bfb4e2fd90eaa9527e71b3c865ada1c56
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-07-31 11:09:21 +0300

    [Update] events/model.py code style

--- di_website/events/models.py
+++ di_website/events/models.py
@@ -1,31 +1,31 @@
 from django.db import models
-from wagtail.admin.edit_handlers import (FieldPanel, InlinePanel,
-                                         MultiFieldPanel, StreamFieldPanel)
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.contrib.contenttypes.models import ContentType
+
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, StreamFieldPanel
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 
 from di_website.common.base import OtherPage, StandardPage, get_paginator_range
 from di_website.common.blocks import BaseStreamBlock
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.contrib.contenttypes.models import ContentType
 
 from taggit.models import Tag, TaggedItemBase
 
-""" Content of each Event that makes up events list """
-
 
 class EventPage(StandardPage):
-
-    template = "events/events_page.html"
+    """ Content of each event """
 
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    location = models.CharField(
-        max_length=100, help_text='Physical location of event')
+    location = models.CharField(max_length=100, help_text='Physical location of event')
 
     body = StreamField(BaseStreamBlock(), verbose_name="Page Body", blank=True)
     other_pages_heading = models.CharField(
-        blank=True, max_length=255, verbose_name='Heading', default='Other pages in this section')
+        blank=True,
+        max_length=255,
+        verbose_name='Heading',
+        default='Other pages in this section'
+    )
 
     content_panels = StandardPage.content_panels + [
         MultiFieldPanel([
@@ -33,30 +33,28 @@ class EventPage(StandardPage):
             FieldPanel('end_time'),
             FieldPanel('location')
         ], heading='Event Details'),
-
         StreamFieldPanel('body'),
         MultiFieldPanel([
             FieldPanel('other_pages_heading'),
             InlinePanel('other_pages', label='Other Pages')
         ], heading='Other Pages')
-
     ]
 
     parent_page_types = ['EventIndexPage']
 
     class Meta:
-        verbose_name = "Event"
-        verbose_name_plural = "Events"
+        verbose_name = "Event Page"
 
-""" List of all events that have been created from events page """
+
 class EventIndexPage(StandardPage):
+    """ List of all events that have been created from events page """
     body = StreamField(BaseStreamBlock(), verbose_name="Page Body", blank=True)
 
     content_panels = StandardPage.content_panels + [
         StreamFieldPanel('body'),
     ]
 
-    def get_context(self,request):
+    def get_context(self, request):
         context = super(EventIndexPage, self).get_context(request)
         page = request.GET.get('page', None)
         events = EventPage.objects.live()

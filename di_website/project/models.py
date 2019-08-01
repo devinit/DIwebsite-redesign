25acf3da4d3973f4dbb6e72cfbeda0a1410e603e
David Ebukali <davide@devinit.org>
2019-08-01 15:27:44 +0300

    Create project page fields

--- di_website/project/models.py
+++ di_website/project/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.admin.edit_handlers import (StreamFieldPanel)
+from wagtail.core.fields import StreamField
+
+from di_website.common.base import StandardPage
+from di_website.common.blocks import BaseStreamBlock
+
+class ProjectPage(StandardPage):
+    body = StreamField(
+        BaseStreamBlock(),
+        verbose_name="Page Body",
+        null=True,
+        blank=True
+    )
+
+    content_panels = StandardPage.content_panels + [
+        StreamFieldPanel('body'),
+    ]

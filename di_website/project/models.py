738d0a846728f52bb45be935ee883f8792064289
David Ebukali <davide@devinit.org>
2019-08-06 18:20:00 +0300

    Add related content fields to project model

--- di_website/project/models.py
+++ di_website/project/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
-from wagtail.admin.edit_handlers import (StreamFieldPanel)
-from wagtail.core.fields import StreamField
+from modelcluster.fields import ParentalKey
 
-from di_website.common.base import StandardPage
+from wagtail.admin.edit_handlers import (
+    StreamFieldPanel,
+    InlinePanel,
+    PageChooserPanel,
+    MultiFieldPanel,
+    FieldPanel
+)
+
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Orderable, Page
+
+from di_website.common.base import StandardPage, OtherPage
 from di_website.common.blocks import BaseStreamBlock
+from di_website.common.constants import MAX_RELATED_LINKS
 
 class ProjectPage(StandardPage):
     body = StreamField(
@@ -13,7 +24,23 @@ class ProjectPage(StandardPage):
         null=True,
         blank=True
     )
+    other_pages_heading = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Section Title'
+    )
 
     content_panels = StandardPage.content_panels + [
         StreamFieldPanel('body'),
+        MultiFieldPanel([
+            FieldPanel('other_pages_heading'),
+            InlinePanel('project_related_links', label="Related links", max_num=3)
+        ], heading='Other Pages')
+    ]
+
+class ProjectPageRelatedLink(OtherPage):
+    page = ParentalKey(Page, related_name='project_related_links', on_delete=models.CASCADE)
+    panels = [
+        PageChooserPanel('other_page')
     ]

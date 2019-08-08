752b4a2016046c858186e0e0dbe9ca41e518d342
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-08-08 14:45:04 +0300

    [Update] use mixins in Project module

--- di_website/project/models.py
+++ di_website/project/models.py
@@ -10,19 +10,15 @@ from wagtail.admin.edit_handlers import (
     FieldPanel
 )
 from wagtail.core.fields import StreamField
-from wagtail.core.models import Orderable, Page
+from wagtail.core.models import Page
 
-from di_website.common.base import StandardPage, OtherPage
+from di_website.common.base import hero_panels
+from di_website.common.mixins import BaseStreamBodyMixin, HeroMixin, OtherPageMixin
 from di_website.common.blocks import BaseStreamBlock
 from di_website.common.constants import MAX_RELATED_LINKS
 
-class ProjectPage(StandardPage):
-    body = StreamField(
-        BaseStreamBlock(),
-        verbose_name="Page Body",
-        null=True,
-        blank=True
-    )
+
+class ProjectPage(BaseStreamBodyMixin, HeroMixin, Page):
     other_pages_heading = models.CharField(
         max_length=255,
         null=True,
@@ -30,16 +26,20 @@ class ProjectPage(StandardPage):
         verbose_name='Section Title'
     )
 
-    content_panels = StandardPage.content_panels + [
+    content_panels = Page.content_panels + [
+        hero_panels(),
         StreamFieldPanel('body'),
         MultiFieldPanel([
             FieldPanel('other_pages_heading'),
-            InlinePanel('project_related_links', label="Related links", max_num=3)
+            InlinePanel('project_related_links',
+                        label="Related links", max_num=MAX_RELATED_LINKS)
         ], heading='Other Pages')
     ]
 
-class ProjectPageRelatedLink(OtherPage):
-    page = ParentalKey(Page, related_name='project_related_links', on_delete=models.CASCADE)
+
+class ProjectPageRelatedLink(OtherPageMixin):
+    page = ParentalKey(
+        Page, related_name='project_related_links', on_delete=models.CASCADE)
     panels = [
         PageChooserPanel('other_page')
     ]

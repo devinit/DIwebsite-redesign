f8c419e59fbb2f7a0567ebaa10fe8c41dbb45528
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-08-12 11:41:08 +0300

    [New] other pages secrtion on places_page

--- di_website/place/models.py
+++ di_website/place/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
-from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    PageChooserPanel,
+    StreamFieldPanel
+)
 
 from di_website.common.base import hero_panels
 from di_website.common.mixins import BaseStreamBodyMixin, HeroMixin
 from .blocks import PlaceStreamBlock
+
+from modelcluster.fields import ParentalKey
 
 
 class PlacesPage(BaseStreamBodyMixin, HeroMixin, Page):
@@ -19,11 +27,21 @@ class PlacesPage(BaseStreamBodyMixin, HeroMixin, Page):
         null=True,
         blank=True
     )
+    other_pages_heading = models.CharField(
+        blank=True,
+        max_length=255,
+        verbose_name='Heading',
+        default='More about'
+    )
 
     content_panels = Page.content_panels + [
         hero_panels(),
         StreamFieldPanel('body'),
-        StreamFieldPanel('places')
+        StreamFieldPanel('places'),
+        MultiFieldPanel([
+            FieldPanel('other_pages_heading'),
+            InlinePanel('other_pages', label='Related pages')
+        ], heading='Other Pages/Related Links')
     ]
 
     # TODO: set Where we Work as parent page

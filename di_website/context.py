e818fef61c4f19f983da0e1965b827a44c5151a6
Naphlin Peter Akena <starnapho@gmail.com>
2019-08-20 16:54:31 +0300

    Added image srcs sizes to context

--- di_website/context.py
+++ di_website/context.py
@@ -14,6 +14,7 @@ def globals(request):
             'hero_srcs': 'fill-400x200 1w, fill-500x280 400w, fill-780x230 500w, fill-1000x435 780w, fill-1350x435 1000w, fill-1800x430 1350w',
             'banner_srcs': 'fill-460x260 460w',
             'card_srcs': 'fill-370x150 1w, fill-470x150 400w, fill-750x150 500w, fill-270x150 780w, fill-270x150 1000w, fill-385x150 1350w',
+            'image_srcs': 'width-270 300w, width-370 400w, width-470 500w, width-750 780w, width-750 780w, width-800 800w',
             'blog_classname': 'BlogArticlePage',
             'event_classname': 'EventPage',
             'news_classname': 'NewsStoryPage'

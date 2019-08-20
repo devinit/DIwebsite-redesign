e9a294cdcbd9463f73a01563f624c1cc7bb56edd
Naphlin Peter Akena <starnapho@gmail.com>
2019-08-20 16:45:32 +0300

    Refactor, more generic model from existing code

--- di_website/general/models.py
+++ di_website/general/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from di_website.home.models import StandardPage
-from di_website.ourteam.models import TeamMemberPage, OurTeamPage
-from di_website.blog.models import BlogIndexPage
+from di_website.common.blocks import DocumentBoxSectionBlock, DuoContentStreamBlock
+from di_website.common.mixins import (
+    TeamMemberStoryMixin, 
+    HeroMixin, 
+    BaseStreamBodyMixin
+)
+
+from di_website.common.base import team_member_story_panels, hero_panels
 
 from wagtail.admin.edit_handlers import (
     FieldPanel,
@@ -11,46 +16,45 @@ from wagtail.admin.edit_handlers import (
 from wagtail.admin.edit_handlers import (
     FieldPanel,
     MultiFieldPanel,
-    PageChooserPanel
+    StreamFieldPanel,
+    InlinePanel,
 )
 
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+
 # Create your models here.
-class Strategy(StandardPage):
+class General(HeroMixin,TeamMemberStoryMixin,BaseStreamBodyMixin,Page):
 
+    """
+    General page that that can be used with strategy  and accounts pages
+    """
 
-    template = 'general/general_page.html'
-
-    team_member_quote = models.TextField(
-        null=True, 
-        blank=False,
-        verbose_name='Quote from team member')
-
-    quote_owner = models.ForeignKey(TeamMemberPage,        
-        null=True,
-        blank=False,
-        on_delete=models.SET_NULL,
-        related_name='+' )
-
-
-    team_page = models.ForeignKey(
-        OurTeamPage,        
-        null=True,
-        blank=False,
-        on_delete=models.SET_NULL,
-        related_name='+' )
+    template = 'general/general_page.html'  
     
-    team_stories = models.ForeignKey(
-        BlogIndexPage,        
-        null=True,
-        blank=False,
-        on_delete=models.SET_NULL,
-        related_name='+' )
+    downloads = StreamField([
+        ('downloads',DocumentBoxSectionBlock()),], verbose_name="Downloads Section",)
+    
+    content = StreamField(DuoContentStreamBlock(),blank=True)
+    other_pages_heading = models.CharField(
+        blank=True,
+        max_length=255,
+        verbose_name='Heading',
+        default='More about'
+    )
 
-    content_panels = StandardPage.content_panels + [
-        MultiFieldPanel([
-            FieldPanel('team_member_quote'),
-            PageChooserPanel('quote_owner','ourteam.TeamMemberPage'),
-            PageChooserPanel('team_page','ourteam.OurTeamPage'),
-            PageChooserPanel('team_stories','blog.BlogIndexPage')
-        ],heading='Team member quote',classname="collapsible collapsed")
+    content_panels = Page.content_panels + [
+       hero_panels(),
+       StreamFieldPanel('body'),
+       team_member_story_panels(),
+       StreamFieldPanel('content'),
+       StreamFieldPanel('downloads'),
+       MultiFieldPanel([
+            FieldPanel('other_pages_heading'),
+            InlinePanel('other_pages', label='Related pages')
+        ], heading='Other Pages/Related Links'),
     ]
+
+    class Meta:
+        verbose_name='General Page'
+

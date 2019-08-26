45d0fd8358f8d82aa74fefb0eabd91c877be76d3
David Ebukali <davide@devinit.org>
2019-08-27 02:46:24 +0300

    Create snippet and display values using custom tag

--- di_website/workforus/models.py
+++ di_website/workforus/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.core.fields import StreamField
+from wagtail.core.fields import StreamField, RichTextField
+from wagtail.core import blocks
 from wagtail.admin.edit_handlers import (
     FieldPanel,
     InlinePanel,
@@ -10,12 +11,31 @@ from wagtail.admin.edit_handlers import (
     StreamFieldPanel
 )
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.snippets.models import register_snippet
 
 from di_website.common.base import hero_panels
 from di_website.common.mixins import BaseStreamBodyMixin, HeroMixin
 from .blocks import BenefitsStreamBlock
 
 from modelcluster.fields import ParentalKey
+
+
+@register_snippet
+class Values(models.Model):
+    title = models.TextField(null=True, blank=True, verbose_name='Value Name',)
+    paragraph = models.TextField(max_length=255, verbose_name='Description',)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('paragraph'),
+    ]
+
+    class Meta():
+        verbose_name = 'Our Value'
+        verbose_name_plural = 'Our Values'
+
+    def __str__(self):
+        return self.title
 
 
 class WorkForUsPage(BaseStreamBodyMixin, HeroMixin, Page):
@@ -50,6 +70,12 @@ class WorkForUsPage(BaseStreamBodyMixin, HeroMixin, Page):
         blank=True
     )
 
+    values_text = models.TextField(
+        blank=True,
+        max_length=255,
+        verbose_name='Brief text for values section',
+    )
+
     content_panels = Page.content_panels + [
         hero_panels(),
         StreamFieldPanel('body'),
@@ -57,4 +83,5 @@ class WorkForUsPage(BaseStreamBodyMixin, HeroMixin, Page):
         FieldPanel('gdr_policy'),
         StreamFieldPanel('benefits'),
         StreamFieldPanel('logos'),
+        FieldPanel('values_text'),
     ]

05f1162af82a84c6408aeec7f00eb1dc8da88bb1
Naphlin Peter Akena <starnapho@gmail.com>
2019-09-03 17:26:26 +0300

    Added code to post new tickets

--- di_website/contactus/utils.py
+++ di_website/contactus/utils.py
@@ -1,6 +1,32 @@
+import requests
+import os
+from django.conf import settings
+import json
 
-class HubSpotHelper:
+END_POINT = 'https://api.hubapi.com/crm-objects/v1/objects/tickets?hapikey=' + \
+    settings.HUBSPOT_API_KEY
+
+
+def create_new_ticket(payload):
     """
-        Helper methods to help post forms to HubSpot CRM
+    Helper methods to help post forms to HubSpot CRM
     """
-    pass
+
+    try:
+        data = json.dumps(payload)
+        response = requests.post(
+            END_POINT,
+            headers={'Content-Type': 'application/json'},
+            data=data)
+
+        response.raise_for_status()
+        # Check that response is 200, otherwise log failed posting
+    except requests.exceptions.HTTPError as http_err:
+        print(f'HTTP error occurred with hubspot: {http_err}')
+    except Exception as err:
+
+        # TODO Route to 500 error page from here
+
+        print(f'Other error occurred with hubspot: {err}')
+    else:
+        pass

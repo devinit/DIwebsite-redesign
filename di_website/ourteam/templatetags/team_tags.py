b9af1b3e5205b3a1bccc36facbe89eb3816a8b7f
Alex Miller <alex.k.miller@gmail.com>
2019-09-17 15:18:25 -0400

    Functioning team tags

--- di_website/ourteam/templatetags/team_tags.py
+++ di_website/ourteam/templatetags/team_tags.py
@@ -13,8 +13,8 @@ def user_content(author_page):
     """
     return chain(
         BlogArticlePage.objects.filter(internal_author_page=author_page).live(),
-        BlogArticlePage.objects.filter(other_authors__contains=[{'type': 'internal_author', 'value': author_page.pk}]).live(),
-        PublicationPage.objects.filter(authors__contains=[{'type': 'internal_author', 'value': author_page.pk}]).live(),
-        ShortPublicationPage.objects.filter(authors__contains=[{'type': 'internal_author', 'value': author_page.pk}]).live(),
-        LegacyPublicationPage.objects.filter(authors__contains=[{'type': 'internal_author', 'value': author_page.pk}]).live()
+        BlogArticlePage.objects.filter(other_authors__contains="\"value\": {}".format(author_page.pk)).live(),
+        PublicationPage.objects.filter(authors__contains="\"value\": {}".format(author_page.pk)).live(),
+        ShortPublicationPage.objects.filter(authors__contains="\"value\": {}".format(author_page.pk)).live(),
+        LegacyPublicationPage.objects.filter(authors__contains="\"value\": {}".format(author_page.pk))
     )

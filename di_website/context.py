72c4a5948ca6a40c9b55cba9fa8be0945d8f8a52
Alex Miller <alex.k.miller@gmail.com>
2019-10-15 10:46:35 -0400

    Edit context

--- di_website/context.py
+++ di_website/context.py
@@ -13,7 +13,7 @@ def globals(request):
             'site_name': request.site.site_name or settings.WAGTAIL_SITE_NAME,
             'large_hero_srcs': 'fill-1000x700 780w, fill-1350x700 1000w, fill-1800x700 1350w',
             'hero_srcs': 'fill-400x200 1w, fill-500x280 400w, fill-780x230 500w, fill-1000x435 780w, fill-1350x435 1000w, fill-1800x430 1350w',
-            'banner_srcs': 'fill-460x260 460w',
+            'banner_srcs': 'width-460 460w',
             'card_srcs': 'fill-370x150 1w, fill-470x150 400w, fill-750x150 500w, fill-270x150 780w, fill-270x150 1000w, fill-385x150 1350w',
             'image_srcs': 'width-270 300w, width-370 400w, width-470 500w, width-750 780w, width-750 780w, width-800 800w',
             'blog_classname': 'BlogArticlePage',

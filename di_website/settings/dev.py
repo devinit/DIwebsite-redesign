a380db77555a022e19879f0bfde9f1563bb9b7b1
Naphlin Peter Akena <starnapho@gmail.com>
2019-10-18 11:50:21 +0300

    Moved email backend config to base

--- di_website/settings/dev.py
+++ di_website/settings/dev.py
@@ -9,8 +9,6 @@ SECRET_KEY = '3bxacdim+p6h*6-j7(zq$t4#4=vzu97+xzeb9-=0rqbh+_o#_d'
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
 
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND') or 'django.core.mail.backends.console.EmailBackend'
-
 INSTALLED_APPS = INSTALLED_APPS+[
     'debug_toolbar',
 ]

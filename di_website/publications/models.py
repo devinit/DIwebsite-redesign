5c6e85a4c112fb8a2c4aeeaa716f8d43f8435029
Alex Miller <alex.k.miller@gmail.com>
2019-10-23 11:45:19 +0300

    Index raw content

--- di_website/publications/models.py
+++ di_website/publications/models.py
@@ -40,7 +40,7 @@ from di_website.downloads.utils import DownloadsPanel
 from taggit.models import Tag, TaggedItemBase
 
 from .mixins import (
-    FlexibleContentMixin, UniquePageMixin, PageSearchMixin, ParentPageSearchMixin,
+    FlexibleContentMixin, UniquePageMixin, PageSearchMixin, LegacyPageSearchMixin, ParentPageSearchMixin,
     PublishedDateMixin, UUIDMixin, ReportChildMixin, FootnoteMixin)
 from .utils import (
     ContentPanel, PublishedDatePanel, WagtailImageField,
@@ -642,7 +642,7 @@ class PublicationAppendixPage(HeroMixin, ReportChildMixin, FlexibleContentMixin,
         return self.data_downloads.all()
 
 
-class LegacyPublicationPage(HeroMixin, PublishedDateMixin, PageSearchMixin, FootnoteMixin, Page):
+class LegacyPublicationPage(HeroMixin, PublishedDateMixin, LegacyPageSearchMixin, FootnoteMixin, Page):
 
     class Meta:
         verbose_name = 'Legacy Publication'

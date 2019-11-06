2b50b8f84e1a7591459940635f468b61aa4952dd
Alex Miller <alex.k.miller@gmail.com>
2019-11-06 14:08:27 -0500

    Start of data section management

--- di_website/common/management/commands/datasectionexcel.py
+++ di_website/common/management/commands/datasectionexcel.py
@@ -1,17 +1,12 @@
-"""Management command that fixes imported WP content."""
+"""Management command that loads datasets."""
 
-import json
-import os
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from di_website.ourteam.models import TeamMemberPage
-
-import requests
 import pandas as pd
 import numpy as np
 
+from di_website.datasection.models import DataSource
 
 class Command(BaseCommand):
     """Management command that that loads metadata from excel and creates new pages for datasection."""
@@ -22,8 +17,35 @@ class Command(BaseCommand):
 
         raw_data = 'https://docs.google.com/spreadsheets/d/1pDbdncnm1TF41kJJX2WjZ2Wq9juOvUqU/export?format=xlsx&id=1pDbdncnm1TF41kJJX2WjZ2Wq9juOvUqU'
 
-        dataset = pd.read_excel(raw_data, sheet_name='Dataset')
         source = pd.read_excel(raw_data, sheet_name='Source Data')
+        dataset = pd.read_excel(raw_data, sheet_name='Dataset')
         figures = pd.read_excel(raw_data, sheet_name='Figures')
+
+        skip = True
+        for source_row in source.iterrows():
+            if skip:
+                skip = False
+            else:
+                source_dict = source_row[1].to_dict()
+                source_check = DataSource.objects.filter(title=source_dict['Source title'])
+                if not source_check:
+                    new_source = DataSource(
+                        title=source_dict['Source title'],
+                        description=source_dict['Long description of the data source'],
+                        organisation=source_dict['Organisation '],
+                        link_to_metadata=source_dict['Link to the source'],
+                        geography=source_dict['Geography information'],
+                        link_to_data=source_dict['Link to the source']
+                    )
+                    new_source.save()
+
+        skip = True
+        for dataset_row in dataset.iterrows():
+            if skip:
+                skip = False
+            else:
+                dataset_dict = source_row[1].to_dict()
+                import pdb; pdb.set_trace()
+
 
         self.stdout.write(self.style.SUCCESS('Called successfully'))

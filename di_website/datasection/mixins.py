ea7d5ad29d8e01b9f7296a76a0ad86e07453d700
Alex Miller <alex.k.miller@gmail.com>
2019-11-07 12:03:46 -0500

    Textblocks and extra fields

--- di_website/datasection/mixins.py
+++ di_website/datasection/mixins.py
@@ -2,9 +2,10 @@ from django.db import models
 from datetime import datetime
 
 from wagtail.core.fields import StreamField
-from wagtail.core.blocks import CharBlock, PageChooserBlock, RichTextBlock, StructBlock, URLBlock
+from wagtail.core.blocks import CharBlock, PageChooserBlock, TextBlock, StructBlock, URLBlock
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
 
 class DataSetMixin(models.Model):
     class Meta():
@@ -13,7 +14,11 @@ class DataSetMixin(models.Model):
     parent_page_types = ['datasection.DataSetListing']
     subpage_types = []
 
+    dataset_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
+    dataset_title = models.TextField(unique=True)
     release_date = models.DateField(default=datetime.now)
+    xlsx_link = models.URLField(blank=True, null=True)
+    csv_link = models.URLField(blank=True, null=True)
     authors = StreamField([
         ('internal_author', PageChooserBlock(
             required=False,
@@ -29,12 +34,16 @@ class DataSetMixin(models.Model):
     ], blank=True)
     meta_data = StreamField(
         [
-            ('description', RichTextBlock(required=True)),
-            ('provenance', RichTextBlock()),
-            ('variables', RichTextBlock()),
-            ('geography', RichTextBlock()),
-            ('licence', RichTextBlock()),
-            ('citation', RichTextBlock())
+            ('description', TextBlock(required=True)),
+            ('provenance', TextBlock()),
+            ('variables', TextBlock()),
+            ('geography', TextBlock()),
+            ('geograpic_coding', TextBlock()),
+            ('unit', TextBlock()),
+            ('internal_notes', TextBlock()),
+            ('lead_analyst', TextBlock()),
+            ('licence', TextBlock()),
+            ('citation', TextBlock())
         ],
         verbose_name='Content',
         help_text='A description is expected, but only one of each shall be shown'
@@ -48,7 +57,7 @@ class DataSetSourceMixin(models.Model):
         abstract = True
 
     source = models.ForeignKey(
-        'datasection.DataSource', null=True, blank  =True, on_delete=models.SET_NULL,
+        'datasection.DataSource', null=True, blank =True, on_delete=models.SET_NULL,
         related_name='+', verbose_name='Data Source')
 
     panels = [SnippetChooserPanel('source')]

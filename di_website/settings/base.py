0864448cdcdf5bdd592238389818a6cbddb6a5eb
Alex Miller <alex.k.miller@gmail.com>
2019-12-02 15:08:11 -0500

    Those are for Django compressor, for another feature

--- di_website/settings/base.py
+++ di_website/settings/base.py
@@ -241,13 +241,6 @@ STATICFILES_DIRS = [
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-COMPRESS_OFFLINE = True
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-COMPRESS_CSS_HASHING_METHOD = 'content'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/assets/'
 

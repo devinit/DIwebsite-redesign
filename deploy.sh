467aa806410668c1d18c5909e52edb48d2113c52
Alex Miller <alex.k.miller@gmail.com>
2019-12-03 15:55:05 -0500

    N and SSL

--- deploy.sh
+++ deploy.sh
@@ -171,7 +171,7 @@ function start_link_checker_processes {
 
 function enable_https_configs {
 
-    if [ "$ENVIROMENT" == 'production' ]; then
+    if [ "$ENVIRONMENT" == 'production' ]; then
         echo 'include /etc/nginx/conf.d/django_https;' >> $APP_DIR"/config/nginx/django.conf"
     fi
 }

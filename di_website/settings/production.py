4840782e6727c87cfc450129ced5ae48c66645df
Naphlin Peter Akena <starnapho@gmail.com>
2019-12-03 18:32:45 +0300

    Added https settings

--- di_website/settings/production.py
+++ di_website/settings/production.py
@@ -3,8 +3,18 @@ from decouple import config
 
 DEBUG = False
 
-SECRET_KEY = config('SECRET_KEY', '3bxacdim+p6h*6-j7(zq$t4#4=vzu97+xzeb9-=0rqbh+_o#_d')
-ALLOWED_HOSTS = ['127.0.0.1', '159.65.56.142', 'localhost', 'devinit.org', 'www.devinit.org']
+SECRET_KEY = config('SECRET_KEY',
+                    '3bxacdim+p6h*6-j7(zq$t4#4=vzu97+xzeb9-=0rqbh+_o#_d')
+ALLOWED_HOSTS = [
+    '127.0.0.1', '159.65.56.142', 'localhost', 'devinit.org',
+    'www.devinit.org', 'staging.devinit.org'
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = not DEBUG
+CSRF_COOKIE_SECURE = not DEBUG
+SESSION_COOKIE_SECURE = not DEBUG
 
 try:
     from .local import *

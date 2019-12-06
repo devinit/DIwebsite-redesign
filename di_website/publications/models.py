8ef1a659fad75ece4aaa8edce0f3c9d31a7bb3f1
Alex Miller <alex.k.miller@gmail.com>
2019-12-06 08:32:35 -0500

    Turn around filtering

--- di_website/publications/models.py
+++ di_website/publications/models.py
@@ -217,8 +217,9 @@ class PublicationIndexPage(HeroMixin, Page):
             story_list.sort(key=lambda x: x.published_date, reverse=True)
 
         promos = get_search_promotions(search_filter)
-        promo_pages = [promo.page.specific for promo in promos if promo.page not in story_list]
+        promo_pages = [promo.page.specific for promo in promos]
         if promo_pages:
+            story_list = [story for story in story_list if story not in promo_pages]
             story_list = list(chain(promo_pages, story_list))
 
         paginator = Paginator(story_list, MAX_PAGE_SIZE)

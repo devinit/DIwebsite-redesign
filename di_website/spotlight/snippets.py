013b910123a29bcd129849b27049b02e8cb03ad4
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-01-16 19:26:01 +0300

    Replace streamfield with snippets

--- di_website/spotlight/snippets.py
+++ di_website/spotlight/snippets.py
@@ -6,6 +6,51 @@ from wagtail.search import index
 
 from wagtail.snippets.models import register_snippet
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+
+@register_snippet
+class Spotlight(ClusterableModel):
+    name = models.CharField(max_length=200)
+
+    panels = [FieldPanel('name')]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Spotlight"
+        verbose_name_plural = "Spotlights"
+
+
+@register_snippet
+class SpotlightTheme(index.Indexed, ClusterableModel):
+    name = models.CharField(max_length=200)
+    spotlight = models.ForeignKey(
+        Spotlight,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="The spotlight this theme belongs to"
+    )
+
+    panels = [
+        FieldPanel('name'),
+        SnippetChooserPanel('spotlight')
+    ]
+
+    search_fields = [
+        index.SearchField('name'),
+        index.RelatedFields('spotlight', [index.SearchField('name')])
+    ]
+
+    def __str__(self):
+        return self.name + ' : ' + self.spotlight.name if self.spotlight else self.name
+
+    class Meta:
+        verbose_name = "Spotlight Theme"
+        verbose_name_plural = "Spotlight Themes"
 
 
 @register_snippet
@@ -40,3 +85,61 @@ class SpotlightColour(ClusterableModel):
     class Meta:
         verbose_name = "Spotlight Colour"
         verbose_name_plural = "Spotlight Colours"
+
+
+@register_snippet
+class SpotlightIndicator(index.Indexed, ClusterableModel):
+    ddw_id = models.CharField(max_length=255)
+    name = models.TextField()
+    description = models.TextField(help_text='A description of this indicator', null=True, blank=True)
+    theme = models.ForeignKey(
+        SpotlightTheme,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    source = models.ForeignKey(
+        SpotlightSource,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    color = models.ForeignKey(
+        SpotlightColour,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    start_year = models.IntegerField()
+    end_year = models.IntegerField()
+    range = models.CharField(max_length=100, null=True, blank=True, help_text='The range of values shown on the legend')
+    value_prefix = models.CharField(max_length=100, null=True, blank=True)
+    value_suffix = models.CharField(max_length=100, null=True, blank=True)
+    tooltip_template = models.TextField(help_text='Text for the tooltip.Template strings can be used to substitute values e.g. {name}')
+
+    panels = [
+        FieldPanel('ddw_id'),
+        FieldPanel('name'),
+        FieldPanel('description'),
+        SnippetChooserPanel('theme'),
+        SnippetChooserPanel('source'),
+        SnippetChooserPanel('color'),
+        FieldPanel('start_year'),
+        FieldPanel('end_year'),
+        FieldPanel('range'),
+        FieldPanel('value_prefix'),
+        FieldPanel('value_suffix'),
+        FieldPanel('tooltip_template')
+    ]
+
+    search_fields = [index.SearchField('ddw_id'), index.SearchField('name')]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Spotlight Indicator"
+        verbose_name_plural = "Spotlight Indicators"

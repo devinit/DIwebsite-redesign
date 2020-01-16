abbe5669c863d73ffb2487e77e24b177fa1b317d
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-01-16 19:26:01 +0300

    Replace streamfield with snippets

--- di_website/spotlight/models.py
+++ di_website/spotlight/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
-from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
-from .blocks import ThemeBlock
 from .snippets import SpotlightSource, SpotlightColour
 
 
@@ -14,10 +13,14 @@ class SpotlightPage(Page):
 
     parent_page_types = ['datasection.DataSectionPage']
 
-    themes = StreamField([
-        ('theme', ThemeBlock())
-    ])
+    meta = models.ForeignKey(
+        'spotlight.Spotlight',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     content_panels = Page.content_panels + [
-        StreamFieldPanel('themes')
+        SnippetChooserPanel('meta')
     ]

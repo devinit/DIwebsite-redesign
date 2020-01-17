bb4c1b64a14ac0096fec223b1884e26c31dd7398
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-01-17 10:36:56 +0300

    Refactor command code

--- di_website/common/management/commands/import_spotlight_data.py
+++ di_website/common/management/commands/import_spotlight_data.py
@@ -15,6 +15,11 @@ class Command(BaseCommand):
         parser.add_argument('branch', nargs='?', type=str, default='master')
 
     def handle(self, *args, **options):
+        self.import_uganda_themes()
+
+    def import_uganda_themes(self):
+        """Import uganda themes from the csv file
+        """
         # Find Spotlight on Uganda Page
         try:
             uganda = Spotlight.objects.filter(slug='spotlight-uganda')[0]
@@ -22,15 +27,22 @@ class Command(BaseCommand):
             uganda = Spotlight(name='Spotlight on Uganda', slug='spotlight-uganda')
 
         SpotlightTheme.objects.filter(spotlight=uganda).delete()
-        # Theme Indicators
+        # Spotlight Themes
         try:
-            theme_file_name = 'uganda-theme.csv'
-            data = pandas.read_csv(theme_file_name)
-            for _, j in data.iterrows():
-                theme = SpotlightTheme(name=j['name'], spotlight=uganda)
-                theme.save()
+            theme_file_name = 'spotlight-uganda-theme.csv'
+            self.create_themes_from_csv(theme_file_name, uganda)
             print('Uganda theme data successfully imported')
-        except FileNotFoundError: # FIXME: proper error handling
+        except FileNotFoundError:
             print('No import of Uganda theme data done. File "theme.csv" not found')
 
-        # os.remove(indicator_file_name)
+    def create_themes_from_csv(self, file_name, spotlight):
+        """Create theme objects from processed csv file
+
+        Arguments:
+            file_name {string} -- The name of the csv file that contains the theme data
+            spotlight {Spotlight} -- The Spotlight model object to relate to the theme
+        """
+        data = pandas.read_csv(file_name)
+        for _, j in data.iterrows():
+            theme = SpotlightTheme(name=j['name'], slug=j['id'], spotlight=spotlight)
+            theme.save()

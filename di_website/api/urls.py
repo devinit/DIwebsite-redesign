47bfe490824d4fb4d6c9b7e50d49ed0784edc990
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-01-20 12:12:30 +0300

    Add spotlight pages API

--- di_website/api/urls.py
+++ di_website/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from di_website.api.views import spotlights_navigation_view, footer_view
+from di_website.api.views import footer_view, spotlights_navigation_view, spotlight_pages_view
 
 urlpatterns = [
     path('spotlights/navigation/', spotlights_navigation_view, name='spotlight-navigation'),
+    path('spotlights/pages/', spotlight_pages_view, name='spotlight-pages'),
     path('footer/', footer_view)
 ]

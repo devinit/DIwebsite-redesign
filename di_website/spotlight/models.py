e7b4368415e83b5f3f6eaedc86f63981831c6235
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-01-31 12:48:36 +0300

    Add country_code to SpotlightPage

--- di_website/spotlight/models.py
+++ di_website/spotlight/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
 from .snippets import SpotlightSource, SpotlightColour
@@ -13,6 +14,7 @@ class SpotlightPage(Page):
 
     parent_page_types = ['datasection.DataSectionPage']
 
+    country_code = models.CharField(max_length=100, help_text='e.g. UG, KE', default='')
     meta = models.ForeignKey(
         'spotlight.Spotlight',
         null=True,
@@ -22,5 +24,8 @@ class SpotlightPage(Page):
     )
 
     content_panels = Page.content_panels + [
-        SnippetChooserPanel('meta')
+        MultiFieldPanel([
+            FieldPanel('country_code')
+        ], heading='Settings'),
+        SnippetChooserPanel('meta'),
     ]

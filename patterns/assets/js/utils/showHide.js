6547e46d32fa0246e49689a1f2a01e74d3d22494
Ben Coleman <ben@bteam.co>
2020-02-04 15:05:27 +0000

    show hide

--- patterns/assets/js/utils/showHide.js
+++ patterns/assets/js/utils/showHide.js
@@ -3,6 +3,9 @@ import $ from 'jquery';
 export default function showHide (trigger, target, targetActive) {
     $(trigger).on('click', function(e) {
 		e.preventDefault();
+		var labelinactive = $(this).data('labelinactive'),
+			labelactive = $(this).data('labelactive');
 		$(target).toggleClass(targetActive);
+		$(trigger).text( $(trigger).text() == labelinactive ? labelactive : labelinactive);
 	});
 }

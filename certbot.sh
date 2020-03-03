9586d939094c9f1479069a4493707925d25e2008
Alex Miller <alex.k.miller@gmail.com>
2020-03-03 10:57:03 -0500

    Missing certbot fix

--- certbot.sh
+++ certbot.sh
@@ -7,8 +7,8 @@ result="$(echo $renewal_result | grep -o Congratulations )"
 #If successful copy certs to the correct folder and refresh nginx docker node
 if [ $result ]; then
     cd $working_directory
-    cp -f  /etc/letsencrypt/live/staging.devinit.org/privkey.pem config/ssl/
-    cp -f /etc/letsencrypt/live/staging.devinit.org/fullchain.pem config/ssl/
+    cp -f  /etc/letsencrypt/live/devinit.org/privkey.pem ssl/
+    cp -f /etc/letsencrypt/live/devinit.org/fullchain.pem ssl/
 
     command docker-compose exec nginx  nginx -s  reload
 fi

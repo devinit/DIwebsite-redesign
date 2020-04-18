3a0230b5887d57dbe3417bbfb7866aa901681049
David Ebukali <debukali@gmail.com>
2020-04-18 13:59:53 +0300

    Update country spotlights model to match new designs

--- di_website/spotlight/blocks.py
+++ di_website/spotlight/blocks.py
@@ -11,23 +11,13 @@ from di_website.common.constants import RICHTEXT_FEATURES_NO_FOOTNOTES
 
 
 class CountryInfoBlock(StructBlock):
-    title = CharBlock()
-    spotlight_description = StreamBlock([
-        ('description', RichTextBlock(
-            icon='fa-paragraph',
-            template='blocks/paragraph_block.html',
-            features=RICHTEXT_FEATURES_NO_FOOTNOTES
-        ))
-    ], blank=True, max_num=2)
     spotlight_page = StreamBlock([
         ('add_spotlight_page', PageChooserBlock(required=False, target_model='spotlight.SpotlightPage')),
-        ('add_location_comparison_page', PageChooserBlock(required=False, target_model='spotlight.SpotlightLocationComparisonPage'))
     ],
         blank=True,
         max_num=2,
         block_counts={
-            'add_spotlight_page': {'max_num': 1},
-            'add_location_comparison_page': {'max_num': 1}
+            'add_spotlight_page': {'max_num': 2},
         }
     )
     background_theme = ChoiceBlock(choices=[

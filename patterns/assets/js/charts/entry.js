f93179677e9859b1aef3dafc2f4fa01eda8b02e1
Russell Kirkland <russell@mashandgravy.co.uk>
2020-06-10 10:13:54 +0100

    Updated chart with prototype drop down button functionality

--- patterns/assets/js/charts/entry.js
+++ patterns/assets/js/charts/entry.js
@@ -24,7 +24,82 @@ export default function entry() {
 
     const el = $('#tester');
     d3.json(el.first().data('url')).then(d => {
-        Plotly.plot(el[0], d);
+        const plot = Plotly.newPlot('tester', d).then(p => {
+
+            Plotly.relayout('tester', {
+                updatemenus: [{
+                    y: 0.8,
+                    yanchor: 'top',
+                    buttons: [{
+                        method: 'restyle',
+                        args: ['line.color', 'red'],
+                        label: 'red'
+                    }, {
+                        method: 'restyle',
+                        args: ['line.color', 'blue'],
+                        label: 'blue'
+                    }, {
+                        method: 'restyle',
+                        args: ['line.color', 'green'],
+                        label: 'green'
+                    }]
+                }]
+            });
+
+        });
+
+        // function makeTrace(i) {
+        //     return {
+        //         y: Array.apply(null, Array(10)).map(() => Math.random()),
+        //         line: {
+        //             shape: 'spline' ,
+        //             color: 'red'
+        //         },
+        //         visible: i === 0,
+        //         name: 'Data set ' + i,
+        //     };
+        // }
+
+        // Plotly.newPlot('tester', [0, 1, 2, 3].map(makeTrace), {
+        //     updatemenus: [{
+        //         y: 0.8,
+        //         yanchor: 'top',
+        //         buttons: [{
+        //             method: 'restyle',
+        //             args: ['line.color', 'red'],
+        //             label: 'red'
+        //         }, {
+        //             method: 'restyle',
+        //             args: ['line.color', 'blue'],
+        //             label: 'blue'
+        //         }, {
+        //             method: 'restyle',
+        //             args: ['line.color', 'green'],
+        //             label: 'green'
+        //         }]
+        //     }, {
+        //         y: 1,
+        //         yanchor: 'top',
+        //         buttons: [{
+        //             method: 'restyle',
+        //             args: ['visible', [true, false, false, false]],
+        //             label: 'Data set 0'
+        //         }, {
+        //             method: 'restyle',
+        //             args: ['visible', [false, true, false, false]],
+        //             label: 'Data set 1'
+        //         }, {
+        //             method: 'restyle',
+        //             args: ['visible', [false, false, true, false]],
+        //             label: 'Data set 2'
+        //         }, {
+        //             method: 'restyle',
+        //             args: ['visible', [false, false, false, true]],
+        //             label: 'Data set 3'
+        //         }]
+        //     }],
+        // });
+
     }).catch(error => {
         console.log(error);
     });

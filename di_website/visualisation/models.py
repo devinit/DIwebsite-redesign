4954b61fd9c63991c79f033dbba6c1123dfffca9
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-06-11 15:49:43 +0300

    Add basic ChartPage

--- di_website/visualisation/models.py
+++ di_website/visualisation/models.py
@@ -9,6 +9,7 @@ class VisualisationsPage(Page):
     Parent page for all visualisations
     """
     parent_page_types = ['home.HomePage']
+    subpage_types = ['visualisation.ChartPage']
 
     class Meta:
         verbose_name = 'Visualisations Page'
@@ -20,5 +21,21 @@ class VisualisationsPage(Page):
         return redirect(self.get_parent().url)
 
     def serve_preview(self, request, mode_name):
-        self.title = self.get_title()
+        return self.get_parent().serve(request)
+
+
+class ChartPage(Page):
+    parent_page_types = [VisualisationsPage]
+    subpage_types = []
+
+    class Meta:
+        verbose_name = 'Chart Page'
+
+    def get_sitemap_urls(self, request):
+        return []
+
+    def serve(self, request, *args, **kwargs):
+        return redirect(self.get_parent().url)
+
+    def serve_preview(self, request, mode_name):
         return self.get_parent().serve(request)

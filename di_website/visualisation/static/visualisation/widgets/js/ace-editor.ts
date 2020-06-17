2f271530de4f396337726ea457b95f0b5519c2e5
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-06-17 19:34:15 +0300

    Revert "Remove typescript"
    This reverts commit 343ea980dbdbc135c18aec1843c51fbfa047a2e5.

---
+++ di_website/visualisation/static/visualisation/widgets/js/ace-editor.ts
@@ -0,0 +1,48 @@
+declare const ace: any; // TODO: find proper types
+declare const Plotly: any; // TODO: use proper types
+
+const renderChart = (options: string, element: HTMLElement) => {
+    const { data, layout } = JSON.parse(options);
+
+    return Plotly.newPlot(element, data, layout);
+}
+
+const initPlotlyPreview = (widgetID: string, options: string) => {
+    const previewNode = document.getElementById(`${widgetID}-plotly-preview`);
+    if (previewNode) {
+        try {
+            renderChart(options, previewNode);
+
+            return {
+                onUpdate: (options: string) => {
+                    renderChart(options, previewNode);
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return null;
+}
+
+const initAceEditor = (widgetID: string) => {
+    if (widgetID) {
+        const editorNode = document.getElementById(`${widgetID}-ace-editor`);
+        const inputNode = document.getElementById(widgetID) as HTMLInputElement;
+        if (editorNode && inputNode) {
+            const editor = ace.edit(editorNode);
+            editor.setTheme("ace/theme/monokai"); //TODO: set theme dynamically
+            editor.session.setMode("ace/mode/json"); //TODO: set mode dynamically
+
+            const preview = initPlotlyPreview(widgetID, inputNode.value);
+
+            editor.getSession().on('change', () => {
+                inputNode.value = editor.getSession().getValue();
+                if (preview) {
+                    preview.onUpdate(inputNode.value);
+                }
+            });
+        }
+    }
+}

dc769e72f93ab108c50728b984f1a3a7bef3fba2
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-06-17 20:09:48 +0300

    Add webpack support

--- di_website/visualisation/static/visualisation/widgets/js/ace-editor.js
+++ di_website/visualisation/static/visualisation/widgets/js/ace-editor.js
@@ -1,40 +1,101 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/visualisation/widgets/ace-editor.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/visualisation/widgets/ace-editor.ts":
+/*!*************************************************!*\
+  !*** ./src/visualisation/widgets/ace-editor.ts ***!
+  \*************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
 "use strict";
-const renderChart = (options, element) => {
-    const { data, layout } = JSON.parse(options);
-    return Plotly.newPlot(element, data, layout);
-};
-const initPlotlyPreview = (widgetID, options) => {
-    const previewNode = document.getElementById(`${widgetID}-plotly-preview`);
-    if (previewNode) {
-        try {
-            renderChart(options, previewNode);
-            return {
-                onUpdate: (options) => {
-                    renderChart(options, previewNode);
-                }
-            };
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-    return null;
-};
-const initAceEditor = (widgetID) => {
-    if (widgetID) {
-        const editorNode = document.getElementById(`${widgetID}-ace-editor`);
-        const inputNode = document.getElementById(widgetID);
-        if (editorNode && inputNode) {
-            const editor = ace.edit(editorNode);
-            editor.setTheme("ace/theme/monokai"); //TODO: set theme dynamically
-            editor.session.setMode("ace/mode/json"); //TODO: set mode dynamically
-            const preview = initPlotlyPreview(widgetID, inputNode.value);
-            editor.getSession().on('change', () => {
-                inputNode.value = editor.getSession().getValue();
-                if (preview) {
-                    preview.onUpdate(inputNode.value);
-                }
-            });
-        }
-    }
-};
+eval("\nconst renderChart = (options, element) => {\n    const { data, layout } = JSON.parse(options);\n    return Plotly.newPlot(element, data, layout);\n};\nconst initPlotlyPreview = (widgetID, options) => {\n    const previewNode = document.getElementById(`${widgetID}-plotly-preview`);\n    if (previewNode) {\n        try {\n            renderChart(options, previewNode);\n            return {\n                onUpdate: (options) => {\n                    renderChart(options, previewNode);\n                }\n            };\n        }\n        catch (error) {\n            console.log(error);\n        }\n    }\n    return null;\n};\nconst initAceEditor = (widgetID) => {\n    if (widgetID) {\n        const editorNode = document.getElementById(`${widgetID}-ace-editor`);\n        const inputNode = document.getElementById(widgetID);\n        if (editorNode && inputNode) {\n            const editor = ace.edit(editorNode);\n            editor.setTheme(\"ace/theme/monokai\"); //TODO: set theme dynamically\n            editor.session.setMode(\"ace/mode/json\"); //TODO: set mode dynamically\n            const preview = initPlotlyPreview(widgetID, inputNode.value);\n            editor.getSession().on('change', () => {\n                inputNode.value = editor.getSession().getValue();\n                if (preview) {\n                    preview.onUpdate(inputNode.value);\n                }\n            });\n        }\n    }\n};\n\n\n//# sourceURL=webpack:///./src/visualisation/widgets/ace-editor.ts?");
+
+/***/ })
+
+/******/ });
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/visualisation/widgets/ace-editor.ts",
+  target: "web",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "di_website/visualisation/static/visualisation/widgets/js"),
+    filename: "ace-editor.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.css$/,
+        loader: "css-loader",
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./di_website/visualisation/static/visualisation/widgets/css/ace-editor.css",
+    }),
+  ],
+};

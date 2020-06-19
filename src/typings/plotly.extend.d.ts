794d4bd75d7cfa3d22d6461eb1e78c4b7cdfed11
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-06-19 12:35:28 +0300

    Load charts as modules based on chart type

--- src/typings/plotly.extend.d.ts
+++ src/typings/plotly.extend.d.ts
@@ -1,8 +1,8 @@
 import 'plotly.js';
 
-declare module "plotly.js" {
-    /**
-     *  registers individual modules for custom import
-     */
-    export const register: (modules: any[]) => void;
+declare module 'plotly.js' {
+  /**
+   *  registers individual modules for custom import
+   */
+  export const register: (modules: any[]) => void; // eslint-disable-line @typescript-eslint/no-explicit-any
 }
---
+++ src/typings/plotly.modules.d.ts
@@ -0,0 +1,27 @@
+declare module 'plotly.js-cartesian-dist' {
+  export = Plotly;
+}
+
+declare module 'plotly.js-basic-dist' {
+  export = Plotly;
+}
+
+declare module 'plotly.js-geo-dist' {
+  export = Plotly;
+}
+
+declare module 'plotly.js-gl3d-dist' {
+  export = Plotly;
+}
+
+declare module 'plotly.js-gl2d-dist' {
+  export = Plotly;
+}
+
+declare module 'plotly.js-mapbox-dist' {
+  export = Plotly;
+}
+
+declare module 'plotly.js-finance-dist' {
+  export = Plotly;
+}

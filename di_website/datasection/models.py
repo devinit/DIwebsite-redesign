29cd432809b85e63047a8d976c2c7ff0ee5006a2
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-06-29 17:49:12 +0300

    Add ShortPublicationPage a child of DataSectionPage

--- di_website/datasection/models.py
+++ di_website/datasection/models.py
@@ -175,7 +175,12 @@ class DataSectionPage(SectionBodyMixin, TypesetBodyMixin, HeroMixin, Page):
     ]
 
     parent_page_types = ['home.HomePage']
-    subpage_types = ['general.General', 'datasection.DataSetListing', 'spotlight.CountrySpotlight']
+    subpage_types = [
+        'general.General',
+        'datasection.DataSetListing',
+        'spotlight.CountrySpotlight',
+        'publications.ShortPublicationPage'
+    ]
 
     class Meta:
         verbose_name = "Data Section Page"

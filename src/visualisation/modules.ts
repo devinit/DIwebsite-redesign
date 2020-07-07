68a6f22a1e131bfa130bee74e5e4f5b109ec9337
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-07-08 00:45:20 +0300

    Use specific type-based modules to reduce bundle size

--- src/visualisation/modules.ts
+++ src/visualisation/modules.ts
@@ -1,68 +1,29 @@
 import Plotly from 'plotly.js';
+import { PlotlyCustom } from './plotly.custom';
 
 // Get and return the correct plotly module based on chart type
-export const loadPlotlyCode = async (data: Plotly.Data[]): Promise<typeof Plotly> => {
-  const chartBundles = {
-    basicCharts: {
-      types: ['bar', 'scatter', 'pie'],
-      dist: () => import('plotly.js-basic-dist'),
-    },
-    cartesianCharts: {
-      types: [
-        'box',
-        'heatmap',
-        'histogram',
-        'histogram2d',
-        'histogram2dcontour',
-        'image',
-        'contour',
-        'scatterternary',
-        'violin',
-      ],
-      dist: () => import('plotly.js-cartesian-dist'),
-    },
-    financeCharts: {
-      types: ['histogram', 'funnelarea', 'ohlc', 'candlestick', 'funnel', 'waterfall', 'indicator'],
-      dist: () => import('plotly.js-finance-dist'),
-    },
-    geoCharts: {
-      types: ['scattergeo', 'choropleth'],
-      dist: () => import('plotly.js-geo-dist'),
-    },
-    geo2dCharts: {
-      types: ['scattergl', 'splom', 'pointcloud', 'heatmapgl', 'contourgl', 'parcoords'],
-      dist: () => import('plotly.js-gl2d-dist'),
-    },
-    geo3dCharts: {
-      types: ['scatter3d', 'surface', 'mesh3d', 'isosurface', 'volume', 'cone', 'streamtube'],
-      dist: () => import('plotly.js-gl3d-dist'),
-    },
-    mapboxCharts: {
-      types: ['scattermapbox', 'choroplethmapbox', 'densitymapbox'],
-      dist: () => import('plotly.js-mapbox-dist'),
-    },
-  };
-
-  const isChartType = (chartTypes: string[]) => {
-    return chartTypes.filter((chart) => data.find((_data) => _data.type === chart)).length;
-  };
-
-  for (const bundle of Object.values(chartBundles)) {
-    // basic doesn't allow groupby transforms, so test that first
-    let hasGroupedByTransform = false;
-    try {
-      if (data[0].transforms) {
-        hasGroupedByTransform = data[0].transforms[0].type === 'groupby';
+export const loadPlotlyCode = async (data: Plotly.Data[]): Promise<PlotlyCustom> => {
+  const chartTypes: Plotly.PlotType[] = data
+    .map((trace) => trace.type)
+    .reduce<Plotly.PlotType[]>((prev, curr: Plotly.PlotType) => {
+      if (prev.indexOf(curr) === -1) {
+        return prev.concat([curr]);
       }
-    } catch (e) {}
 
-    // if no groupedby transform present, try to get the module based on matching type
-    if (!hasGroupedByTransform) {
-      if (isChartType(bundle.types)) {
-        return await bundle.dist();
-      }
-    }
-  }
+      return prev;
+    }, []);
 
-  return await import('plotly.js');
+  const { newPlot, purge, register, react } = await import('./plotly.custom');
+
+  chartTypes.forEach(async (type) => {
+    register([await import(`plotly.js/lib/${type}`)]);
+  });
+  register([
+    require('plotly.js/lib/aggregate'),
+    require('plotly.js/lib/filter'),
+    require('plotly.js/lib/groupby'),
+    require('plotly.js/lib/sort'),
+  ]);
+
+  return { newPlot, purge, react };
 };

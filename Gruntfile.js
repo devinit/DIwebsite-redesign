aff79ccca2cfef1c637a98f16fb81cc5ad28fa0f
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-07-07 12:42:03 +0300

    Remove AceEditor from webpack config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,36 @@
-"use strict";
-const webpack = require("webpack");
-const webpackConfig = require("./webpack.config");
-const merge = require("webpack-merge");
+'use strict';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpackConfig = require('./webpack.config');
+const merge = require('webpack-merge');
 
-const webpackConfigProduction = webpackConfig.map(config => merge(config, {
+const webpackConfigProduction = webpackConfig.map((config) =>
+  merge(config, {
     devtool: false,
-    mode: 'production'
-}));
+    mode: 'production',
+  }),
+);
 
-module.exports = function(grunt) {
-    grunt.initConfig({
-
-        watch: {
-            update: {
-                files: [ "./src/**/*", "src/**/*" ],
-                tasks: [ "webpack:develop" ],
-                options: {
-                    debounceDelay: 250
-                }
-            }
+module.exports = function (grunt) {
+  grunt.initConfig({
+    watch: {
+      update: {
+        files: ['./src/**/*', 'src/**/*'],
+        tasks: ['webpack:develop'],
+        options: {
+          debounceDelay: 250,
         },
+      },
+    },
 
-        webpack: {
-            develop: webpackConfig,
-            build: webpackConfigProduction
-        },
-    });
+    webpack: {
+      develop: webpackConfig,
+      build: webpackConfigProduction,
+    },
+  });
 
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-webpack");
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-webpack');
 
-    grunt.registerTask("default", [ "watch" ]);
-    grunt.registerTask(
-        "build",
-        "Compiles all the assets and copies the files to the dist directory.",
-        [ "webpack:build" ]
-    );
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', 'Compiles all the assets and copies the files to the dist directory.', ['webpack:build']);
 };

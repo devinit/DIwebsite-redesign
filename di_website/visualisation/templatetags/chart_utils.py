e5c3616c96bb888626c24e7c2d68563a65bfbd22
Russell Kirkland <russell@mashandgravy.co.uk>
2020-07-07 09:09:15 +0100

    Add filter to calculate padding top requirement for fallback images

--- di_website/visualisation/templatetags/chart_utils.py
+++ di_website/visualisation/templatetags/chart_utils.py
@@ -10,3 +10,12 @@ def as_json(value):
         return json.dumps(value)
     except Exception:
         return 'Invalid JSON data'
+
+
+@register.filter
+def padding_by_ratio(image):
+    try:
+        ratio = image.height / image.width
+        return 'padding-top: %s%%;' % round(ratio * 100, 2)
+    except Exception:
+        return ''

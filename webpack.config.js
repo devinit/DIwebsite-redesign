946e248d067b6a82db813998c2f78d6619c9dcf2
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-07-07 12:42:03 +0300

    Remove AceEditor from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const sharedConfig = {
   target: 'web',
@@ -27,21 +27,6 @@ const sharedConfig = {
   },
 };
 
-const wagtailAceEditorConfig = {
-  ...sharedConfig,
-  entry: './src/visualisation/widgets/ace-editor.ts',
-  output: {
-    path: path.resolve(__dirname, 'di_website/visualisation/static/visualisation/widgets/js'),
-    filename: 'ace-editor.js',
-    library: 'WagtailAceEditor',
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: './di_website/visualisation/static/visualisation/widgets/css/ace-editor.css',
-    }),
-  ],
-};
-
 const chartsConfig = {
   ...sharedConfig,
   entry: ['@babel/polyfill', './src/visualisation/index.ts'],
@@ -55,4 +40,4 @@ const chartsConfig = {
 };
 chartsConfig.module.rules[0].loader = 'babel-loader';
 
-module.exports = [wagtailAceEditorConfig, chartsConfig];
+module.exports = [chartsConfig];

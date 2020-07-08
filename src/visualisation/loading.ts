b40ea245e4c340c657ed3762e7dc79d0ae54c953
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-07-08 09:42:51 +0300

    Include loading indicator in template

--- src/visualisation/loading.ts
+++ src/visualisation/loading.ts
@@ -1,13 +1,9 @@
 // add loading on chart init
 export const addLoading = (element: HTMLElement): void => {
-  const loading = document.createElement('div');
-  loading.innerHTML = '<div class="chart-loading__block">' + '<div></div><div></div><div></div><div></div></div>';
-  loading.classList.add('chart-loading');
   element.classList.add('chart-container--loading');
-  element.prepend(loading);
 };
 
-// remove loading from inited chart
+// remove loading from initiated chart
 export const removeLoading = (element: HTMLElement): void => {
   element.classList.remove('chart-container--loading');
   const loadingIndicator = element.querySelector('.chart-loading');

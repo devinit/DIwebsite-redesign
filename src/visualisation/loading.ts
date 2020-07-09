87d6403063568f992603253ac531c32f83af515f
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-07-09 16:45:47 +0300

    Remove redundant code & comments

--- src/visualisation/loading.ts
+++ src/visualisation/loading.ts
@@ -1,9 +1,7 @@
-// add loading on chart init
 export const addLoading = (element: HTMLElement): void => {
   element.classList.add('chart-container--loading');
 };
 
-// remove loading from initiated chart
 export const removeLoading = (element: HTMLElement): void => {
   element.classList.remove('chart-container--loading');
   const loadingIndicator = element.querySelector('.chart-loading');
--- src/visualisation/options.ts
+++ src/visualisation/options.ts
@@ -1,14 +1,13 @@
 import { CalcData } from './types';
 
 // Assign an option to a select node
-export const assignOption = (selectNode: HTMLSelectElement, value: string): void => {
+export const addOptionToSelectNode = (selectNode: HTMLSelectElement, value: string): void => {
   const currentOption = document.createElement('option');
   currentOption.text = value;
   selectNode.appendChild(currentOption);
 };
 
-// Take an array of strings and assign as options to a select node
-export const assignOptions = (selectNode: HTMLSelectElement, options: string[]): void => {
+export const addOptionsToSelectNode = (selectNode: HTMLSelectElement, options: string[]): void => {
   if (!options.length) {
     return;
   }
@@ -16,23 +15,10 @@ export const assignOptions = (selectNode: HTMLSelectElement, options: string[]):
     if (!option) {
       return;
     }
-    assignOption(selectNode, option);
+    addOptionToSelectNode(selectNode, option);
   });
   selectNode.classList.add('data-selector--active');
 };
 
-// get the options from the legend items
-export const getOptions = (legend: HTMLElement, traces: Plotly.Data[]): (string | undefined)[] => {
-  if (legend) {
-    const options: string[] = [];
-    legend.querySelectorAll('.legendtext').forEach((el) => options.push((el as any).dataset.unformatted)); // eslint-disable-line @typescript-eslint/no-explicit-any
-
-    return options;
-  }
-
-  return traces.map((el) => el.name);
-};
-
-export const createOptionsFromLegendData = (calcData: CalcData[][]): string[] => {
-  return calcData.map((item) => item[0].trace.name as string);
-};
+export const createOptionsFromLegendData = (calcData: CalcData[][]): string[] =>
+  calcData.map((item) => item[0].trace.name as string);

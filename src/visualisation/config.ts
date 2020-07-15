391c0f6bfe1bfa308f39756f7f99620fede21954
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-07-15 16:51:41 +0300

    Show legend in downloaded png image

--- src/visualisation/config.ts
+++ src/visualisation/config.ts
@@ -2,12 +2,32 @@ import { Config } from 'plotly.js';
 import { PlotlyEnhancedHTMLElement } from './types';
 const modebarButtons = require('plotly.js/src/components/modebar/buttons'); // eslint-disable-line
 
+const MAX_VERTICAL_LEGEND_ITEMS = 13;
+const MAX_NEAT_HORIZONTAL_LEGEND_ITEMS = 30;
 const onImageClick = modebarButtons.toImage.click;
 modebarButtons.toImage.click = (chartNode: PlotlyEnhancedHTMLElement) => {
   // customise layout before downloading image
   chartNode.layout.title = { text: chartNode.dataset.shareLink };
+  const showLegend = chartNode.layout.showlegend;
+  chartNode.layout.showlegend = true;
+  const defaultLegendOptions = chartNode.layout.legend || {};
+  const visibleLegendItems = chartNode._fullData.filter((data) => data.visible).length;
+  if (visibleLegendItems > MAX_VERTICAL_LEGEND_ITEMS) {
+    chartNode.layout.legend = {
+      ...defaultLegendOptions,
+      orientation: 'h',
+      y: visibleLegendItems > MAX_NEAT_HORIZONTAL_LEGEND_ITEMS ? -0.5 : -0.2,
+      xanchor: 'center',
+      x: 0.5,
+      traceorder: 'reversed',
+    };
+  } else {
+    chartNode.layout.legend = { ...defaultLegendOptions, orientation: 'v', traceorder: 'reversed' };
+  }
   onImageClick(chartNode);
   chartNode.layout.title = { text: '' };
+  chartNode.layout.legend = defaultLegendOptions;
+  chartNode.layout.showlegend = showLegend;
 };
 
 // config object for new plots

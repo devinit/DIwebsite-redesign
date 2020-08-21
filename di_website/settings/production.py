d59a969325ddaff1d3173ac66314bed3af899141
mariamiah <mariah.kakande@gmail.com>
2020-08-21 11:02:38 +0300

    enable https autoredirect staging

--- di_website/settings/production.py
+++ di_website/settings/production.py
@@ -6,8 +6,8 @@ DEBUG = False
 SECRET_KEY = config('SECRET_KEY',
                     '3bxacdim+p6h*6-j7(zq$t4#4=vzu97+xzeb9-=0rqbh+_o#_d')
 ALLOWED_HOSTS = [
-    '127.0.0.1', '159.65.56.142', 'localhost', 'devinit.org',
-    'www.devinit.org', 'staging.devinit.org', 'www.staging.devinit.org'
+    '1 '165.232.38.245',27.0.0.1', '159.65.56.142', 'localhost', 'devinit.org', 'dev.devinit.org', 'www.dev.devinit.org',
+   'www.devinit.org', 'staging.devinit.org', 'www.staging.devinit.org'
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

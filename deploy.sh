b8270a09eba9785b6939267e1e77390c56e57582
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-09-23 03:07:57 +0300

    Revert backup command update

--- deploy.sh
+++ deploy.sh
@@ -106,8 +106,7 @@ function backup_database {
     cd $APP_DIR
 
     log "Starting backup from remote docker machine $(docker-compose ps -q db)"
-    docker-compose exec -T db pg_dump -Fc -U di_website -d di_website -f $current_date.backup
-    docker cp diwebsiteredesign_db_1:$current_date.backup $DATABASE_BACKUP
+    docker-compose exec -T db pg_dump -U di_website -d di_website >  $file_name
 
     log "Database backup completed..."
 

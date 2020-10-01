3f35dd919a8e4514c5a250eaf0f1f7d4e04a49c8
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-10-01 14:14:14 +0300

    Add default  echart options

--- src/visualisation/library/echarts.ts
+++ src/visualisation/library/echarts.ts
@@ -1,6 +1,59 @@
+import deepmerge from 'deepmerge';
 import { EChartOption, ECharts } from 'echarts';
 import { DIChart } from './dicharts';
 import { DIChartEChartOptions } from './utils';
+
+const defaultOptions: EChartOption = {
+  legend: {
+    top: 10,
+    textStyle: {
+      fontFamily: 'Geomanist Regular,sans-serif',
+    },
+  },
+  tooltip: {
+    trigger: 'axis',
+    textStyle: {
+      fontFamily: 'Geomanist Regular,sans-serif',
+    },
+  },
+  toolbox: {
+    showTitle: false,
+    feature: {
+      saveAsImage: {
+        title: 'Save as image',
+        pixelRatio: 2,
+      },
+    },
+    right: 20,
+    tooltip: {
+      show: true,
+      textStyle: {
+        fontFamily: 'Geomanist Regular,sans-serif',
+        formatter: function (param: { title: string }) {
+          return `<div>${param.title}</div>`; // user-defined DOM structure
+        },
+      },
+    },
+  },
+  xAxis: {
+    axisLabel: {
+      fontFamily: 'Geomanist Regular,sans-serif',
+      fontSize: 13,
+    },
+    splitLine: {
+      show: false,
+    },
+  },
+  yAxis: {
+    axisLabel: {
+      fontFamily: 'Geomanist Regular,sans-serif',
+      fontSize: 13,
+    },
+    splitLine: {
+      show: false,
+    },
+  },
+};
 
 export class DIEChart extends DIChart {
   private config: DIChartEChartOptions;
@@ -22,7 +75,8 @@ export class DIEChart extends DIChart {
   };
 
   setOptions = (options: EChartOption): DIEChart => {
-    this.options = options;
+    this.options = deepmerge(defaultOptions, options);
+    console.log(this.options);
 
     return this;
   };

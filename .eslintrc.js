20bc69b28549b96c55891b9724ff40423daa2f3f
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-16 11:00:21 +0300

    Update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'react/prop-types': [0],
     "newline-before-return": "error",
     eqeqeq: ["error", "always"],
     "prefer-template": "error",

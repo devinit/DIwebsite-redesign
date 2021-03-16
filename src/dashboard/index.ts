4fc5aa96736616ff1f4a6816bbed1685ff9db464
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-16 09:13:48 +0300

    Add Dashboard component

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -1,1 +1,8 @@
-console.log('Dashboard is Loading ...');
+import { createElement } from 'react';
+import { render } from 'react-dom';
+import { Dashboard } from './Dashboard';
+
+const rootNode = document.getElementById('root');
+if (rootNode) {
+  render(createElement(Dashboard), rootNode);
+}

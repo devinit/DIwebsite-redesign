30684c12cf64e3986c55b2f3bcc752aa8b358911
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-25 19:43:35 +0300

    Add overhead time chart for Finance

--- src/dashboard/utils/index.ts
+++ src/dashboard/utils/index.ts
@@ -1,3 +1,5 @@
+import { DashboardData } from '../../utils/types';
+
 export const getQuarterYear = (dateString: string): [number, number] => {
   try {
     const date = new Date(dateString);
@@ -10,3 +12,25 @@ export const getQuarterYear = (dateString: string): [number, number] => {
 };
 
 export const colours = ['#6c120a', '#a21e25', '#cd2b2a', '#dc372d', '#ec6250', '#f6b0a0', '#fbd7cb', '#fce3dc'];
+
+export const generateDataset = (data: DashboardData[]) => {
+  // extract unique metrics & dates
+  const metrics = [...new Set(data.map(({ metric }) => metric))];
+  const dates = [...new Set(data.map(({ date }) => date))];
+
+  return dates.map((date) => {
+    const [quarter, year] = getQuarterYear(date);
+    const dataset: Record<string, string | number> = { quarter: `${year} Q${quarter}`, year };
+    metrics.forEach((metric) => {
+      const matchingData = data.find((item) => item.date === date && metric === item.metric);
+      if (matchingData) {
+        dataset[metric] = matchingData.value;
+        if (matchingData.target) {
+          dataset['Target'] = matchingData.target;
+        }
+      }
+    });
+
+    return dataset;
+  });
+};

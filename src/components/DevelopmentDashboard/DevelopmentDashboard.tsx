f50045b90718742adc355df11c833010c1c7261d
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-25 14:08:14 +0300

    Add Demo Charts to Development Dashboard

--- src/components/DevelopmentDashboard/DevelopmentDashboard.tsx
+++ src/components/DevelopmentDashboard/DevelopmentDashboard.tsx
@@ -1,5 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import { DashboardData } from '../../utils/types';
+import { ApacheChart } from '../ApacheChart';
+import { Card } from '../Card';
+import { Grid } from '../Grid';
 import { Section } from '../Section';
 
 type DevelopmentDashboardProps = {
@@ -11,7 +14,36 @@ type DevelopmentDashboardProps = {
 const DevelopmentDashboard: FunctionComponent<DevelopmentDashboardProps> = () => {
   return (
     <Section title="Development & Fundraising" id="development">
-      <div>Content Goes Here</div>
+      <Grid columns={2}>
+        <Card meta="Proportion of staff time spent on projects">
+          <ApacheChart demo options={{}} height="250px" />
+        </Card>
+        <Card meta="Proportion of value of staff time spent on direct & indirect overheads">
+          <ApacheChart demo options={{}} height="250px" />
+        </Card>
+        <Card meta="Personnel costs as a proportion of income (% of target)">
+          <ApacheChart demo options={{}} height="250px" />
+        </Card>
+        <Card meta="Consultant costs %, YTD (excluding GNR)">
+          <ApacheChart demo options={{}} height="250px" type="pie" />
+        </Card>
+      </Grid>
+      <Grid columns={1}>
+        <Card meta="Testing Bar Chart">
+          <ApacheChart demo options={{}} height="250px" type="bar" />
+        </Card>
+      </Grid>
+      <Grid columns={3}>
+        <Card meta="Testing Pie Charts">
+          <ApacheChart demo options={{}} height="250px" type="pie" />
+        </Card>
+        <Card meta="Testing Pie Charts">
+          <ApacheChart demo options={{}} height="250px" type="pie" />
+        </Card>
+        <Card meta="Testing Pie Charts">
+          <ApacheChart demo options={{}} height="250px" type="pie" />
+        </Card>
+      </Grid>
     </Section>
   );
 };

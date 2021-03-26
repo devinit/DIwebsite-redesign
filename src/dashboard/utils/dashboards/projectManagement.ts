6aeb1000269eff2b80a6348682280a02920ee351
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-26 15:34:26 +0300

    Add project spending pie chart

--- src/dashboard/utils/dashboards/projectManagement.ts
+++ src/dashboard/utils/dashboards/projectManagement.ts
@@ -7,7 +7,7 @@ export const projectManagement: DashboardGrid[] = [
     columns: 2,
     content: [
       {
-        id: 'staff',
+        id: 'project-status',
         meta: 'Project Status',
         chart: {
           data: (data: DashboardData[]): React.ReactText[][] =>
@@ -64,6 +64,69 @@ export const projectManagement: DashboardGrid[] = [
 
                       default:
                         return 'Status Unknown';
+                    }
+                  },
+                },
+              },
+            ],
+            /* eslint-enable @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types */
+          },
+        },
+      },
+      {
+        id: 'project-spending',
+        meta: 'Project Spending',
+        chart: {
+          data: (data: DashboardData[]): React.ReactText[][] =>
+            generateArrayDataset(
+              data.filter(({ metric }) =>
+                ['% projects overspending', '% projects underspending', '% projects on track'].includes(metric),
+              ),
+            ),
+          options: {
+            color: colours,
+            tooltip: { trigger: 'item', show: false },
+            legend: { show: false, top: 'auto', bottom: 10 },
+            dataset: {},
+            grid: {
+              left: '3%',
+              right: '4%',
+              top: '3%',
+              containLabel: true,
+            },
+            toolbox: {
+              feature: {
+                saveAsImage: {},
+              },
+            },
+            xAxis: { show: false, type: 'category' },
+            yAxis: { show: false },
+            /* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types */
+            series: [
+              {
+                type: 'pie',
+                id: 'pie',
+                radius: '65%',
+                center: ['50%', '50%'],
+                encode: {
+                  itemName: 'metric',
+                  value: '2020 Q4',
+                  tooltip: '2020 Q4',
+                },
+                label: {
+                  show: true,
+                  formatter: (params: any): string => {
+                    const value = params.value[params.encode.value[0]];
+                    switch (params.name) {
+                      case '% projects overspending':
+                        return `Overspending: ${value}%`;
+                      case '% projects underspending':
+                        return `Underspending: ${value}%`;
+                      case '% projects on track':
+                        return `On Track: ${value}%`;
+
+                      default:
+                        return 'Expenditured Unknown';
                     }
                   },
                 },

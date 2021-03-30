eed8f0a19f75b5b4f40ae12d4350f9f44791827c
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-30 17:10:23 +0300

    Add localforage for offline storage

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -1,6 +1,7 @@
 import { createElement } from 'react';
 import { render } from 'react-dom';
 import { Dashboard } from './Dashboard';
+import './utils/localforage';
 
 const rootNode = document.getElementById('root');
 if (rootNode) {

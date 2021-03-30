135e75ad8ce3292b81dc1526dbaa4efb016c0a75
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-30 12:42:37 +0300

    Fetch data from dashboard API

--- src/dashboard/hooks/data.ts
+++ src/dashboard/hooks/data.ts
@@ -1,34 +1,23 @@
 import { useEffect, useState } from 'react';
 import { DashboardData } from '../../utils/types';
-import * as Papa from 'papaparse';
 
 export const useDashboardData = (): DashboardData[] => {
   const [data, setData] = useState<DashboardData[]>([]);
   useEffect(() => {
-    const dataURL =
-      'https://raw.githubusercontent.com/devinit/DIwebsite-redesign/patch/advanced-chart-resources/data/organisation_dashboard.csv';
-    Papa.parse(dataURL, {
-      download: true,
-      complete: ({ data }) => {
-        const dashboardData = [...data];
-        if (dashboardData && dashboardData.length) {
-          const titleRow = dashboardData.splice(0, 1)[0] as string[];
+    const dataURL = `${window.location.origin}/api/dashboard/data/`;
+    window
+      .fetch(dataURL)
+      .then((response) => response.json())
+      .then(({ data, error }) => {
+        if (error) {
+          console.log('Failed to fetch data:', error);
 
-          setData(
-            dashboardData.map<DashboardData>((row: string[]) => ({
-              metric: row[titleRow.indexOf('Metric')],
-              date: row[titleRow.indexOf('date')],
-              year: parseInt(row[titleRow.indexOf('year')]),
-              quarter: row[titleRow.indexOf('quarter')],
-              value: parseFloat(row[titleRow.indexOf('value')]),
-              department: row[titleRow.indexOf('Department')],
-              narrative: row[titleRow.indexOf('narrative')],
-              target: parseFloat(row[titleRow.indexOf('target')]),
-            })),
-          );
+          return;
         }
-      },
-    });
+        if (data) {
+          setData(data);
+        }
+      });
   }, []);
 
   return data;

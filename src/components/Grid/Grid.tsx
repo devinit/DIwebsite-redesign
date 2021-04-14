8c4999248788f8c6d2a7054f8d2042baf389732e
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-04-14 09:07:14 +0300

    Add className support to dashboard grids

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,11 +1,13 @@
+import classNames from 'classnames';
 import React, { FunctionComponent } from 'react';
 
 type GridProps = {
   columns?: number;
+  className?: string;
 };
 
-const Grid: FunctionComponent<GridProps> = ({ columns, children }) => {
-  return <div className={`l-${columns}up`}>{children}</div>;
+const Grid: FunctionComponent<GridProps> = ({ columns, children, className }) => {
+  return <div className={classNames(`l-${columns}up`, className)}>{children}</div>;
 };
 
 Grid.defaultProps = {

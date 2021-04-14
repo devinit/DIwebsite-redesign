e29ca696f825572d1aa3f3e5410c9c440c516554
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-04-14 09:16:42 +0300

    Hide filter and summary sections

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,13 +1,15 @@
+import classNames from 'classnames';
 import React, { FunctionComponent, ReactText } from 'react';
 
 type SectionProps = {
   title?: ReactText;
   id?: string;
+  className?: string;
 };
 
 const Section: FunctionComponent<SectionProps> = (props) => {
   return (
-    <section className="section" id={props.id}>
+    <section className={classNames('section', props.className)} id={props.id}>
       <div className="row row--narrow">
         {props.title ? <h2 className="section__heading">{props.title}</h2> : null}
 

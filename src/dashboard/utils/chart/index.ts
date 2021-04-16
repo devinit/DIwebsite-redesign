c8e747e547591ddbb1ff3e3fc274e032a265904d
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-04-16 09:40:18 +0300

    Add drill down/up to ratio of staff to leavers chart

--- src/dashboard/utils/chart/index.ts
+++ src/dashboard/utils/chart/index.ts
@@ -1,1 +1,19 @@
 export * from './events';
+
+/**
+ * Use when updating a chart's options
+ * Captures the current options & adds an event listener that will revert to the old options when the canvas is clicked
+ * @param chart echarts.ECharts
+ */
+export const addChartReverseListener = (chart: echarts.ECharts): void => {
+  const currentOptions = chart.getOption();
+  const chartNode = chart.getDom();
+  const canvas = chartNode.getElementsByTagName('canvas')[0];
+  if (canvas) {
+    const onClick = () => {
+      chart.setOption(currentOptions);
+      canvas.removeEventListener('click', onClick);
+    };
+    canvas.addEventListener('click', onClick);
+  }
+};

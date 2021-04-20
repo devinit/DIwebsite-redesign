aa10ce80f289cf299a47b254e883b8cc80cee70e
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-04-20 18:13:40 +0300

    Add seo score and dwell time

--- src/dashboard/utils/dashboards/comms.ts
+++ src/dashboard/utils/dashboards/comms.ts
@@ -3,7 +3,11 @@ import { DashboardData, DashboardGrid } from '../../../utils/types';
 import { getEventHandlers, grid } from '../chart';
 
 const colours = ['#7d4712', '#a85d00', '#df8000', '#f9b865', '#feedd4'];
-const dashboardMetrics = ['Bounce rate on the website (%)'];
+const dashboardMetrics = [
+  'Bounce rate on the website (%)',
+  'Dwell time on the website (minutes)',
+  'Overall on-page SEO score',
+];
 
 export const comms: DashboardGrid[] = [
   {
@@ -25,7 +29,11 @@ export const comms: DashboardGrid[] = [
               formatter: (params: echarts.EChartOption.Tooltip.Format): string => {
                 const { value, seriesName } = params;
 
-                return `${(value as any)[seriesName!]}%`; // eslint-disable-line
+                if (value && seriesName && (value as any)[seriesName]) { // eslint-disable-line
+                  return `${(value as any)[seriesName]}%`; // eslint-disable-line
+                }
+
+                return 'No Data';
               },
             },
             legend: { show: false },
@@ -37,6 +45,72 @@ export const comms: DashboardGrid[] = [
             series: [{ type: 'bar' }],
           },
           ...getEventHandlers(dashboardMetrics[0]),
+        },
+      },
+      {
+        id: 'dwell-time',
+        meta: dashboardMetrics[1],
+        styled: true,
+        chart: {
+          data: (data: DashboardData[]): Record<string, React.ReactText>[] =>
+            getAggregatedDatasetSource(data, Array<string>().concat(dashboardMetrics[1])),
+          options: {
+            color: colours,
+            tooltip: {
+              show: true,
+              trigger: 'item',
+              formatter: (params: echarts.EChartOption.Tooltip.Format): string => {
+                const { value, seriesName } = params;
+
+                if (value && seriesName && (value as any)[seriesName]) { // eslint-disable-line
+                  return `${(value as any)[seriesName]}`; // eslint-disable-line
+                }
+
+                return 'No Data';
+              },
+            },
+            legend: { show: false },
+            dataset: { dimensions: ['year'].concat(dashboardMetrics[1]) },
+            grid,
+            toolbox: { feature: { saveAsImage: {} } },
+            xAxis: { type: 'category' },
+            yAxis: { type: 'value', show: true, splitNumber: 3, axisLabel: { formatter: '{value}' } },
+            series: [{ type: 'bar' }],
+          },
+          ...getEventHandlers(dashboardMetrics[1]),
+        },
+      },
+      {
+        id: 'seo-score',
+        meta: dashboardMetrics[2],
+        styled: true,
+        chart: {
+          data: (data: DashboardData[]): Record<string, React.ReactText>[] =>
+            getAggregatedDatasetSource(data, Array<string>().concat(dashboardMetrics[2])),
+          options: {
+            color: colours,
+            tooltip: {
+              show: true,
+              trigger: 'item',
+              formatter: (params: echarts.EChartOption.Tooltip.Format): string => {
+                const { value, seriesName } = params;
+
+                if (value && seriesName && (value as any)[seriesName]) { // eslint-disable-line
+                  return `${(value as any)[seriesName]}`; // eslint-disable-line
+                }
+
+                return 'No Data';
+              },
+            },
+            legend: { show: false },
+            dataset: { dimensions: ['year'].concat(dashboardMetrics[2]) },
+            grid,
+            toolbox: { feature: { saveAsImage: {} } },
+            xAxis: { type: 'category' },
+            yAxis: { type: 'value', show: true, splitNumber: 3, axisLabel: { formatter: '{value}' } },
+            series: [{ type: 'bar' }],
+          },
+          ...getEventHandlers(dashboardMetrics[2]),
         },
       },
     ],

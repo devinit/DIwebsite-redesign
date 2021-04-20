28a1f3872dc40934f57daeb1a0e46da6c7a38081
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-04-20 12:32:12 +0300

    Remove obsolete code from Dashboard

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,11 +1,6 @@
-import React, { FunctionComponent, useState } from 'react';
-import { Card, CardMetaLarge, CardTitleLarge } from '../components/Card';
+import React, { FunctionComponent } from 'react';
 import { DashboardSection } from '../components/DashboardSection';
 import { DataSystemsDashboard } from '../components/DataSystemsDashboard';
-import { Filter, Option } from '../components/Filter';
-import { Grid } from '../components/Grid';
-import { Section } from '../components/Section';
-import { Quarter } from '../utils/types';
 import { useDashboardData } from './hooks/data';
 import {
   comms,
@@ -15,61 +10,11 @@ import {
   projectManagement,
 } from './utils/dashboards';
 
-const years: Option[] = [
-  { value: 2020, caption: '2020' },
-  { value: 2021, caption: '2021' },
-];
-
-const quarters: Option[] = [
-  { value: '1', caption: 'Q1' },
-  { value: '2', caption: 'Q2' },
-  { value: '3', caption: 'Q3' },
-  { value: '4', caption: 'Q4' },
-];
-
 const Dashboard: FunctionComponent = () => {
   const data = useDashboardData();
-  const [year, setYear] = useState<number>(2020);
-  const [quarter, setQuarter] = useState<Quarter>();
-
-  const onSelectYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setYear(parseInt(event.currentTarget.value));
-  };
-
-  const onSelectQuarter = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setQuarter(parseInt(event.currentTarget.value) as Quarter);
-  };
 
   return (
     <>
-      <Section className="display-none">
-        <div className="highlight">
-          <form className="form">
-            <Filter id="year" label="Year" options={years} onChange={onSelectYear} />
-            <Filter id="quarter" label="Quarter" options={quarters} onChange={onSelectQuarter} />
-          </form>
-        </div>
-      </Section>
-      <Section id="summary" className="display-none">
-        <Grid columns={4}>
-          <Card>
-            <CardMetaLarge>Contract Income Secured</CardMetaLarge>
-            <CardTitleLarge>£85,000</CardTitleLarge>
-          </Card>
-          <Card>
-            <CardMetaLarge>Grant Income Secured</CardMetaLarge>
-            <CardTitleLarge>£185,000</CardTitleLarge>
-          </Card>
-          <Card>
-            <CardMetaLarge>Salary Vs Income</CardMetaLarge>
-            <CardTitleLarge>85%</CardTitleLarge>
-          </Card>
-          <Card>
-            <CardMetaLarge>Staff Vs Leavers</CardMetaLarge>
-            <CardTitleLarge>84/3</CardTitleLarge>
-          </Card>
-        </Grid>
-      </Section>
       <DashboardSection id="finance" title="Finance" department="Finance" data={data} grids={financeDashboard} />
       <DashboardSection id="hr" title="Human Resources" department="HR" data={data} grids={humanResourcesDashboard} />
       <DashboardSection

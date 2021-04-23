5c982c0b46ce49fa818e02ffa44c503474700144
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-04-23 16:49:17 +0300

    Add income at 90%

--- src/dashboard/utils/dashboards/fundraising.ts
+++ src/dashboard/utils/dashboards/fundraising.ts
@@ -3,7 +3,11 @@ import { DashboardData, DashboardGrid, EventOptions } from '../../../utils/types
 import { addChartReverseListener, getEventHandlers, grid, tootipFormatter } from '../chart';
 
 const colours = ['#42184c', '#632572', '#994d98', '#cb98c4', '#ebcfe5'];
-const dashboardMetrics = ['Income secured this quarter', 'Weighted value 50/80% probable pipeline at end of quarter'];
+const dashboardMetrics = [
+  'Income secured this quarter',
+  'Weighted value 50/80% probable pipeline at end of quarter',
+  'Income at 90% at end of quarter (waiting for agreement to be signed for 2021-2023)',
+];
 
 export const fundraising: DashboardGrid[] = [
   {
@@ -209,25 +213,25 @@ export const fundraising: DashboardGrid[] = [
           ...getEventHandlers(dashboardMetrics[0]),
         },
       },
-    ],
-  },
-  {
-    id: '5',
-    columns: 1,
-    className: 'm-pills',
-    content: [
       {
         id: 'grant-income',
-        meta: 'Income at 90% at end of quarter (waiting for agreement to be signed for 2021-2023)',
+        meta: 'Income at 90% at end of quarter',
         styled: true,
-        title: (data: DashboardData[]): React.ReactText => {
-          const currentMetric = 'Income at 90% at end of quarter (waiting for agreement to be signed for 2021-2023)';
-          const metricData = data.filter(
-            ({ metric, year, quarter, category }) =>
-              metric.trim() === currentMetric && category.trim() === 'Grants' && year === 2021 && quarter === 'Q1',
-          );
-
-          return metricData && metricData.length && metricData[0].value ? toPounds(metricData[0].value) : 'None';
+        chart: {
+          data: (data: DashboardData[]): Record<string, React.ReactText>[] =>
+            getAggregatedDatasetSource(data, Array<string>().concat(dashboardMetrics[2])),
+          options: {
+            color: colours,
+            tooltip: { show: true, trigger: 'item', formatter: tootipFormatter({ currency: true }) },
+            legend: { show: false },
+            dataset: { dimensions: ['year'].concat(dashboardMetrics[2]) },
+            grid,
+            toolbox: { feature: { saveAsImage: {} } },
+            xAxis: { type: 'category' },
+            yAxis: { type: 'value', show: true, splitNumber: 3, axisLabel: { formatter: '£{value}' } },
+            series: [{ type: 'bar' }],
+          },
+          ...getEventHandlers(dashboardMetrics[2]),
         },
       },
     ],

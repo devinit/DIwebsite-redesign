f4dfdadd833290c200007b5bd903b934a233f963
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-09 16:17:58 +0300

    Fix optimise toggle

--- src/optimize/index.ts
+++ src/optimize/index.ts
@@ -13,8 +13,8 @@ const setupOptimize = () => {
   const variant = window.google_optimize.get('_aj7TeknQICK1m3pwQRBKw');
   console.log(variant);
 
-  toggleFeature('.optimize-original', !(variant && `${variant}` === '1'));
-  toggleFeature('.optimize-variant', variant && `${variant}` === '1');
+  toggleFeature('.optimize-original', variant ? !(`${variant}` === '1') : true);
+  toggleFeature('.optimize-variant', !!variant && `${variant}` === '1');
 };
 
 const waitForOptimize = () => {

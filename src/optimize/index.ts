cc397072e96b089612daa7b7254cec8317e933fb
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-09 14:03:03 +0300

    Improve google optimise usage

--- src/optimize/index.ts
+++ src/optimize/index.ts
@@ -1,9 +1,7 @@
-declare const google_optimize: { get: (id: string) => string };
-
 window.addEventListener('load', () => {
   const setupOptimize = () => {
-    if (google_optimize) {
-      const variant = google_optimize.get('_aj7TeknQICK1m3pwQRBKw');
+    if (window.google_optimize) {
+      const variant = window.google_optimize.get('_aj7TeknQICK1m3pwQRBKw');
       const params = new URLSearchParams(window.location.search);
       const { origin, pathname } = window.location;
       if (variant && variant === '1' && variant !== params.get('variant')) {
@@ -12,6 +10,11 @@ window.addEventListener('load', () => {
       if (variant && variant === '0' && params.get('variant') === '1') {
         window.location.href = `${origin}${pathname}`;
       }
+      if (!!params.get('variant')) {
+        window.location.href = `${origin}${pathname}`;
+      }
+    } else {
+      console.log('Google Optimize may not be active');
     }
   };
 

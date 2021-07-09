b349f1675add5d1c5ad9a7b9246054f280e817ea
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-09 14:09:43 +0300

    Wait for optimize to be available

--- src/optimize/index.ts
+++ src/optimize/index.ts
@@ -1,22 +1,28 @@
-window.addEventListener('load', () => {
-  const setupOptimize = () => {
-    if (window.google_optimize) {
-      const variant = window.google_optimize.get('_aj7TeknQICK1m3pwQRBKw');
-      const params = new URLSearchParams(window.location.search);
-      const { origin, pathname } = window.location;
-      if (variant && variant === '1' && variant !== params.get('variant')) {
-        window.location.href = `${origin}${pathname}?variant=1`;
-      }
-      if (variant && variant === '0' && params.get('variant') === '1') {
-        window.location.href = `${origin}${pathname}`;
-      }
-      if (!!params.get('variant')) {
-        window.location.href = `${origin}${pathname}`;
-      }
-    } else {
-      console.log('Google Optimize may not be active');
+const setupOptimize = () => {
+  if (window.google_optimize) {
+    const variant = window.google_optimize.get('_aj7TeknQICK1m3pwQRBKw');
+    const params = new URLSearchParams(window.location.search);
+    const { origin, pathname } = window.location;
+    if (variant && variant === '1' && variant !== params.get('variant')) {
+      window.location.href = `${origin}${pathname}?variant=1`;
     }
-  };
+    if (variant && variant === '0' && params.get('variant') === '1') {
+      window.location.href = `${origin}${pathname}`;
+    }
+    if (!!params.get('variant')) {
+      window.location.href = `${origin}${pathname}`;
+    }
+  } else {
+    console.log('Google Optimize may not be active');
+  }
+};
 
-  setupOptimize();
-});
+const waitForOptimize = () => {
+  if (window.google_optimize) {
+    setupOptimize();
+  } else {
+    setTimeout(() => waitForOptimize(), 100);
+  }
+};
+
+waitForOptimize();

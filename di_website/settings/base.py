c3b9740812b69c40a518260b4fe5cd3198d4b1c4
ThatcherK <kaliithatmau@gmail.com>
2021-09-16 09:39:59 +0300

    configure cloudinary

--- di_website/settings/base.py
+++ di_website/settings/base.py
@@ -263,7 +263,7 @@ MEDIA_URL = '/media/'
 # Cloudinary settings
 
 cloudinary.config(
-  cloud_name = os.getenv('CLOUDINARY_NAME'),
+  cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
   api_key = os.getenv('CLOUDINARY_API_KEY'),
   api_secret = os.getenv('CLOUDINARY_API_SECRET')
 )

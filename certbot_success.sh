dbe2603be6b141d9c08653cb8c77f4d118cf06c5
Wakibi Chris <wakibic@gmail.com>
2021-10-26 00:14:28 +0300

    Fix success script

--- certbot_success.sh
+++ certbot_success.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 cp -f /etc/letsencrypt/live/"${SITE_URL}"/privkey.pem /etc/letsencrypt/privkey.pem
 cp -f /etc/letsencrypt/live/"${SITE_URL}"/fullchain.pem /etc/letsencrypt/fullchain.pem

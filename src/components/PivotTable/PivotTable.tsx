778c7c9226745b1e63925e473b37964d3e6ad5ea
Edwin P. Magezi <emagezi@gmail.com>
2021-12-20 16:46:54 +0300

    Get pivot table data from CMS into react component

--- src/components/PivotTable/PivotTable.tsx
+++ src/components/PivotTable/PivotTable.tsx
@@ -2,9 +2,17 @@ import React, { FC } from 'react';
 
 interface PivotTableProps {
   data: Record<string, unknown>[];
+  filters: string[];
+  rowLabel: string;
+  columnLabel: string;
+  cellValue: string;
+  showRowTotal?: boolean;
+  showColumnTotal?: boolean;
 }
 
-const PivotTable: FC<PivotTableProps> = () => {
+const PivotTable: FC<PivotTableProps> = (props) => {
+  console.log(props);
+
   return <div>Table Goes Here!</div>;
 };
 
--- src/plugins/pivottable.ts
+++ src/plugins/pivottable.ts
@@ -5,10 +5,21 @@ import { PivotTable } from '../components/PivotTable';
 export const initPivotTables = function (): void {
   const pivotTables = document.querySelectorAll('.js-pivot-table');
   Array.prototype.forEach.call(pivotTables, (tableWrapper: HTMLDivElement) => {
-    const dataURL = tableWrapper.dataset.url;
+    const { url: dataURL, filters, row, column, cell, rowTotal, columnTotal } = tableWrapper.dataset;
     if (dataURL) {
       window.d3.csv(dataURL, (data) => {
-        render(createElement(PivotTable, { data }), tableWrapper);
+        render(
+          createElement(PivotTable, {
+            data,
+            filters: filters?.split(',') || [],
+            rowLabel: row || '',
+            columnLabel: column || '',
+            showRowTotal: rowTotal === 'True',
+            showColumnTotal: columnTotal === 'True',
+            cellValue: cell || '',
+          }),
+          tableWrapper,
+        );
       });
     }
   });

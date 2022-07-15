99a3fc25894fb2f340a012de5780aa3a18717ff7
Pauline <pauline@DI-UG000174.local>
2022-07-15 12:41:09 +0300

    Add a condition to handle manual selection of related links and automatic when country and topic is selected

--- di_website/publications/models.py
+++ di_website/publications/models.py
@@ -487,17 +487,18 @@ class PublicationPage(
         context = super().get_context(request, *args, **kwargs)
         related_options = request.GET.get('choices', None)
 
-        if related_options == 'Topic':
-            queryset = PublicationPage.objects.filter(topics__in=self.topics)
+
+        if related_options == 'Manual':
             context['related_pages'] = get_related_pages(
-                self, self.publication_related_links.all(), queryset)
+                self, self.publication_related_links.all(), PublicationPage.objects)
         elif related_options == 'Country':
             queryset = PublicationPage.objects.filter(page_countries__in=self.page_countries)
             context['related_pages'] = get_related_pages(
                 self, self.publication_related_links.all(), queryset)
         else:
+            queryset = PublicationPage.objects.filter(topics__in=self.topics)
             context['related_pages'] = get_related_pages(
-                self, self.publication_related_links.all(), PublicationPage.objects)
+                self, self.publication_related_links.all(), queryset)
 
         return context
 

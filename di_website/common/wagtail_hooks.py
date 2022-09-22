33400c5b5c068fd16d102e31714d5c07a58efe02
David Ebukali <debukali@gmail.com>
2022-09-22 11:59:33 +0300

    Use uppercase for related_option_handler when copying pages

--- di_website/common/wagtail_hooks.py
+++ di_website/common/wagtail_hooks.py
@@ -7,3 +7,9 @@ from wagtail.core import hooks
 def global_admin_css():
     """Add /static/css/admin.css to the admin."""
     return format_html('<link rel="stylesheet" href="{}">', static("css/admin.css"))
+
+@hooks.register('before_copy_page')
+def update_related_options_handler(request, page):
+    """Update related_option_handler to uppercase"""
+    page.specific.related_option_handler = page.specific.related_option_handler.upper()
+    return page

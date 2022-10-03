26e6dd323f89eb42855a2f402b3df84606075854
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-10-03 13:26:47 +0300

    Refactor FootnoteEntity

--- src/admin/footnotes/js/blocks/FootnoteEntity.js
+++ src/admin/footnotes/js/blocks/FootnoteEntity.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable react/react-in-jsx-scope */
 import PropTypes from 'prop-types';
 const Component = window.React.Component;
 const Icon = window.wagtail.components.Icon;
@@ -7,94 +9,78 @@ const shortid = require('shortid');
 const $ = window.jQuery;
 
 const getAttributes = (data) => {
-    const icon = ICON;
-    const label = data.text || null;
+  const icon = ICON;
+  const label = data.text || null;
 
-    return {
-        icon,
-        label,
-    };
+  return {
+    icon,
+    label,
+  };
 };
 
 window.footnoteUUIDList = window.footnoteUUIDList || {};
 
 const checkExistingUUID = (entityKey, uuid) => {
+  // check that this uuid doesn't already exist (which would happen with a copy/paste action)
+  let verifiedUUID;
 
-    // check that this uuid doesn't already exist (which would happen with a copy/paste action)
-    let verifiedUUID;
+  if (typeof window.footnoteUUIDList[uuid] === 'undefined') {
+    verifiedUUID = uuid;
+  } else if (window.footnoteUUIDList[uuid] !== entityKey) {
+    verifiedUUID = shortid.generate();
+  } else if (window.footnoteUUIDList[uuid] === entityKey) {
+    verifiedUUID = uuid;
+  }
 
-    if (typeof window.footnoteUUIDList[uuid] === 'undefined') {
-        verifiedUUID = uuid;
-    } else if (window.footnoteUUIDList[uuid] !== entityKey) {
-        verifiedUUID = shortid.generate();
-    } else if (window.footnoteUUIDList[uuid] === entityKey) {
-        verifiedUUID = uuid;
-    }
+  window.footnoteUUIDList[verifiedUUID] = entityKey;
 
-    window.footnoteUUIDList[verifiedUUID] = entityKey;
-    return verifiedUUID;
-}
+  return verifiedUUID;
+};
 
 const handleUUID = (lastChangeType, contentState, entityKey, data) => {
+  let verifiedUUID = data.uuid;
 
-    let verifiedUUID = data.uuid;
+  if (lastChangeType === 'insert-fragment') {
+    verifiedUUID = checkExistingUUID(entityKey, data.uuid);
+  } else {
+    window.footnoteUUIDList[verifiedUUID] = entityKey;
+  }
 
-    if (lastChangeType === 'insert-fragment') {
-        verifiedUUID = checkExistingUUID(entityKey, data.uuid);
-    } else {
-        window.footnoteUUIDList[verifiedUUID] = entityKey;
-    }
-
-    contentState.replaceEntityData(
-        entityKey,
-        {
-            text: data.text,
-            uuid: verifiedUUID
-        }
-    );
-}
+  contentState.replaceEntityData(entityKey, {
+    text: data.text,
+    uuid: verifiedUUID,
+  });
+};
 
 export class FootnoteEntity extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const {
-            contentState,
-            entityKey,
-        } = this.props;
-    }
+    const { contentState, entityKey } = this.props;
+  }
 
-    render() {
-        const {
-            contentState,
-            entityKey,
-        } = this.props;
+  render() {
+    const { contentState, entityKey } = this.props;
 
-        const lastChangeType = this.props.getEditorState().getLastChangeType();
+    const lastChangeType = this.props.getEditorState().getLastChangeType();
 
-        this.props.children = '';
-        const data = contentState.getEntity(entityKey).getData();
-        handleUUID(lastChangeType, contentState, entityKey, data);
+    this.props.children = '';
+    const data = contentState.getEntity(entityKey).getData();
+    handleUUID(lastChangeType, contentState, entityKey, data);
 
-        // TODO: find a way to set the 'showTooltipAt' state of the tooltip entity properly
-        // This .hide() instruction is here as the tooltip entity appears on sibling entities when a footnote is removed
-        $('.Tooltip.Tooltip--top').hide();
+    // TODO: find a way to set the 'showTooltipAt' state of the tooltip entity properly
+    // This .hide() instruction is here as the tooltip entity appears on sibling entities when a footnote is removed
+    $('.Tooltip.Tooltip--top').hide();
 
-        return (
-            <span
-                role="button"
-                className="FootnoteEntity"
-            >
-                <TooltipEntity
-                    {...this.props}
-                    {...getAttributes(data)}
-                />
-            </span>
-        );
-    }
+    return (
+      <span role="button" className="FootnoteEntity">
+        <TooltipEntity {...this.props} {...getAttributes(data)} />
+      </span>
+    );
+  }
 }
 
 FootnoteEntity.propTypes = {
-    entityKey: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
+  entityKey: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
--- src/admin/footnotes/js/decorators/Footnote.js
+++ src/admin/footnotes/js/decorators/Footnote.js
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable react/react-in-jsx-scope */
 import PropTypes from 'prop-types';
-const TooltipEntity = window.draftail.TooltipEntity;
+// const TooltipEntity = window.draftail.TooltipEntity;
 import { FootnoteEntity } from '../blocks/FootnoteEntity';
 
 const Footnote = (props) => {
-  const { entityKey, contentState } = props;
+  // const { entityKey, contentState } = props;
 
   return <FootnoteEntity {...props} />;
 };

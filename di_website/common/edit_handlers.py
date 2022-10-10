504b9e2de7fe6f0c9ebf65088e32fca97c058bf3
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-10-10 12:29:45 +0300

    Fix broken help-icons on help blocks since Wagtail 4

--- di_website/common/edit_handlers.py
+++ di_website/common/edit_handlers.py
@@ -62,5 +62,5 @@ def HelpPanel(
     wrapper_class='help-block help-info'
 ):
     """Define a help text panel."""
-    wrapped_content = '<div class="%s">%s</div>' % (wrapper_class, content)
+    wrapped_content = '<div class="%s"><svg class="icon icon-help" aria-hidden="true"><use href="#icon-help"></use></svg>%s</div>' % (wrapper_class, content)
     return WagtailHelpPanel(content=wrapped_content, template=template, heading=heading, classname=classname)
